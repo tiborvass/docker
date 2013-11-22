moby/moby

Refactor State to be 100% thread safe

state.go
@@ -8,37 +8,74 @@ import (
 )
 
 type State struct {
-	sync.Mutex
-	Running    bool
-	Pid        int
-	ExitCode   int
-	StartedAt  time.Time
-	FinishedAt time.Time
-	Ghost      bool
+	sync.RWMutex
+	running    bool
+	pid        int
+	exitCode   int
+	startedAt  time.Time
+	finishedAt time.Time
+	ghost      bool
 }
 
 // String returns a human-readable description of the state
 func (s *State) String() string {
-	if s.Running {
-		if s.Ghost {
+	s.RLock()
+	defer s.RUnlock()
+
+	if s.running {
+		if s.ghost {
 			return fmt.Sprintf("Ghost")
 		}
-		return fmt.Sprintf("Up %s", utils.HumanDuration(time.Now().Sub(s.StartedAt)))
+		return fmt.Sprintf("Up %s", utils.HumanDuration(time.Now().Sub(s.startedAt)))
 	}
-	return fmt.Sprintf("Exit %d", s.ExitCode)
+	return fmt.Sprintf("Exit %d", s.exitCode)
 }
 
-func (s *State) setRunning(pid int) {
-	s.Running = true
-	s.Ghost = false
-	s.ExitCode = 0
-	s.Pid = pid
-	s.StartedAt = time.Now()
+func (s *State) IsRunning() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.running
 }
 
-func (s *State) setStopped(exitCode int) {
-	s.Running = false
-	s.Pid = 0
-	s.FinishedAt = time.Now()
-	s.ExitCode = exitCode
+func (s *State) IsGhost() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.ghost
+}
+
+func (s *State) GetExitCode() int {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.exitCode
+}
+
+func (s *State) SetGhost(val bool) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.ghost = val
+}
+
+func (s *State) SetRunning(pid int) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.running = true
+	s.ghost = false
+	s.exitCode = 0
+	s.pid = pid
+	s.startedAt = time.Now().UTC()
+}
+
+func (s *State) SetStopped(exitCode int) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.running = false
+	s.pid = 0
+	s.finishedAt = time.Now().UTC()
+	s.exitCode = exitCode
 }

tiborvass/docker

state.go
@@ -8,7 +8,7 @@ import (
 )
 
 type State struct {
-	sync.Mutex
+	sync.RWMutex
 	Running    bool
 	Pid        int
 	ExitCode   int
@@ -19,6 +19,9 @@ type State struct {
 
 // String returns a human-readable description of the state
 func (s *State) String() string {
+	s.RLock()
+	defer s.RUnlock()
+
 	if s.Running {
 		if s.Ghost {
 			return fmt.Sprintf("Ghost")
@@ -28,7 +31,38 @@ func (s *State) String() string {
 	return fmt.Sprintf("Exit %d", s.ExitCode)
 }
 
-func (s *State) setRunning(pid int) {
+func (s *State) IsRunning() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.Running
+}
+
+func (s *State) IsGhost() bool {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.Ghost
+}
+
+func (s *State) GetExitCode() int {
+	s.RLock()
+	defer s.RUnlock()
+
+	return s.ExitCode
+}
+
+func (s *State) SetGhost(val bool) {
+	s.Lock()
+	defer s.Unlock()
+
+	s.Ghost = val
+}
+
+func (s *State) SetRunning(pid int) {
+	s.Lock()
+	defer s.Unlock()
+
 	s.Running = true
 	s.Ghost = false
 	s.ExitCode = 0
@@ -36,7 +70,10 @@ func (s *State) setRunning(pid int) {
 	s.StartedAt = time.Now()
 }
 
-func (s *State) setStopped(exitCode int) {
+func (s *State) SetStopped(exitCode int) {
+	s.Lock()
+	defer s.Unlock()
+
 	s.Running = false
 	s.Pid = 0
 	s.FinishedAt = time.Now()