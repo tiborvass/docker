moby/moby

Removed runconfig.ParseSubcommand
 Removed runconfig.ParseSubcommand, changed it to runconfig.Parse and editted related tests and modules

Signed-off-by: Oh Jinkyun <tintypemolly@gmail.com>

builder/dispatchers.go
@@ -9,11 +9,13 @@ package builder
 
 import (
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
 	"strings"
 
 	"github.com/docker/docker/nat"
 	"github.com/docker/docker/pkg/log"
+	flag "github.com/docker/docker/pkg/mflag"
 	"github.com/docker/docker/runconfig"
 )
 
@@ -172,7 +174,11 @@ func run(b *Builder, args []string, attributes map[string]bool) error {
 		return fmt.Errorf("Please provide a source image with `from` prior to run")
 	}
 
-	config, _, _, err := runconfig.Parse(append([]string{b.image}, args...), nil)
+	runCmd := flag.NewFlagSet("run", flag.ContinueOnError)
+	runCmd.SetOutput(ioutil.Discard)
+	runCmd.Usage = nil
+
+	config, _, _, err := runconfig.Parse(runCmd, append([]string{b.image}, args...), nil)
 	if err != nil {
 		return err
 	}

integration/utils_test.go
@@ -19,6 +19,8 @@ import (
 	"github.com/docker/docker/daemon"
 	"github.com/docker/docker/engine"
 	"github.com/docker/docker/pkg/log"
+	flag "github.com/docker/docker/pkg/mflag"
+	"github.com/docker/docker/pkg/sysinfo"
 	"github.com/docker/docker/runconfig"
 	"github.com/docker/docker/utils"
 )
@@ -248,7 +250,7 @@ func readFile(src string, t *testing.T) (content string) {
 // The caller is responsible for destroying the container.
 // Call t.Fatal() at the first error.
 func mkContainer(r *daemon.Daemon, args []string, t *testing.T) (*daemon.Container, *runconfig.HostConfig, error) {
-	config, hc, _, err := runconfig.Parse(args, nil)
+	config, hc, _, err := parseRun(args, nil)
 	defer func() {
 		if err != nil && t != nil {
 			t.Fatal(err)
@@ -347,4 +349,11 @@ func getImages(eng *engine.Engine, t *testing.T, all bool, filter string) *engin
 	}
 	return images
 
+}
+
+func parseRun(args []string, sysInfo *sysinfo.SysInfo) (*runconfig.Config, *runconfig.HostConfig, *flag.FlagSet, error) {
+	cmd := flag.NewFlagSet("run", flag.ContinueOnError)
+	cmd.SetOutput(ioutil.Discard)
+	cmd.Usage = nil
+	return runconfig.Parse(cmd, args, sysInfo)
 }

runconfig/parse_test.go
@@ -1,10 +1,20 @@
 package runconfig
 
 import (
+	"io/ioutil"
 	"testing"
 
+	flag "github.com/docker/docker/pkg/mflag"
 	"github.com/docker/docker/pkg/parsers"
+	"github.com/docker/docker/pkg/sysinfo"
 )
+
+func parseRun(args []string, sysInfo *sysinfo.SysInfo) (*Config, *HostConfig, *flag.FlagSet, error) {
+	cmd := flag.NewFlagSet("run", flag.ContinueOnError)
+	cmd.SetOutput(ioutil.Discard)
+	cmd.Usage = nil
+	return Parse(cmd, args, sysInfo)
+}
 
 func TestParseLxcConfOpt(t *testing.T) {
 	opts := []string{"lxc.utsname=docker", "lxc.utsname = docker "}
@@ -24,27 +34,27 @@ func TestParseLxcConfOpt(t *testing.T) {
 }
 
 func TestNetHostname(t *testing.T) {
-	if _, _, _, err := Parse([]string{"-h=name", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"-h=name", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"--net=host", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"--net=host", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=bridge", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=bridge", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=none", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=none", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=host", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=host", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
 		t.Fatalf("Expected error ErrConflictNetworkHostname, got: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=container:other", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=container:other", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
 		t.Fatalf("Expected error ErrConflictNetworkHostname, got: %s", err)
 	}
 }

tiborvass/docker

builder/dispatchers.go
@@ -9,11 +9,13 @@ package builder
 
 import (
 	"fmt"
+	"io/ioutil"
 	"path/filepath"
 	"strings"
 
 	"github.com/tiborvass/docker/nat"
 	"github.com/tiborvass/docker/pkg/log"
+	flag "github.com/tiborvass/docker/pkg/mflag"
 	"github.com/tiborvass/docker/runconfig"
 )
 
@@ -176,9 +178,11 @@ func run(b *Builder, args []string, attributes map[string]bool) error {
 		args = append([]string{"/bin/sh", "-c"}, args[0])
 	}
 
-	args = append([]string{b.image}, args...)
+	runCmd := flag.NewFlagSet("run", flag.ContinueOnError)
+	runCmd.SetOutput(ioutil.Discard)
+	runCmd.Usage = nil
 
-	config, _, _, err := runconfig.Parse(args, nil)
+	config, _, _, err := runconfig.Parse(runCmd, append([]string{b.image}, args...), nil)
 	if err != nil {
 		return err
 	}

integration/utils_test.go
@@ -19,6 +19,8 @@ import (
 	"github.com/tiborvass/docker/daemon"
 	"github.com/tiborvass/docker/engine"
 	"github.com/tiborvass/docker/pkg/log"
+	flag "github.com/tiborvass/docker/pkg/mflag"
+	"github.com/tiborvass/docker/pkg/sysinfo"
 	"github.com/tiborvass/docker/runconfig"
 	"github.com/tiborvass/docker/utils"
 )
@@ -248,7 +250,7 @@ func readFile(src string, t *testing.T) (content string) {
 // The caller is responsible for destroying the container.
 // Call t.Fatal() at the first error.
 func mkContainer(r *daemon.Daemon, args []string, t *testing.T) (*daemon.Container, *runconfig.HostConfig, error) {
-	config, hc, _, err := runconfig.Parse(args, nil)
+	config, hc, _, err := parseRun(args, nil)
 	defer func() {
 		if err != nil && t != nil {
 			t.Fatal(err)
@@ -347,4 +349,11 @@ func getImages(eng *engine.Engine, t *testing.T, all bool, filter string) *engin
 	}
 	return images
 
+}
+
+func parseRun(args []string, sysInfo *sysinfo.SysInfo) (*runconfig.Config, *runconfig.HostConfig, *flag.FlagSet, error) {
+	cmd := flag.NewFlagSet("run", flag.ContinueOnError)
+	cmd.SetOutput(ioutil.Discard)
+	cmd.Usage = nil
+	return runconfig.Parse(cmd, args, sysInfo)
 }

runconfig/parse_test.go
@@ -1,10 +1,20 @@
 package runconfig
 
 import (
+	"io/ioutil"
 	"testing"
 
+	flag "github.com/tiborvass/docker/pkg/mflag"
 	"github.com/tiborvass/docker/pkg/parsers"
+	"github.com/tiborvass/docker/pkg/sysinfo"
 )
+
+func parseRun(args []string, sysInfo *sysinfo.SysInfo) (*Config, *HostConfig, *flag.FlagSet, error) {
+	cmd := flag.NewFlagSet("run", flag.ContinueOnError)
+	cmd.SetOutput(ioutil.Discard)
+	cmd.Usage = nil
+	return Parse(cmd, args, sysInfo)
+}
 
 func TestParseLxcConfOpt(t *testing.T) {
 	opts := []string{"lxc.utsname=docker", "lxc.utsname = docker "}
@@ -24,27 +34,27 @@ func TestParseLxcConfOpt(t *testing.T) {
 }
 
 func TestNetHostname(t *testing.T) {
-	if _, _, _, err := Parse([]string{"-h=name", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"-h=name", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"--net=host", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"--net=host", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=bridge", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=bridge", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=none", "img", "cmd"}, nil); err != nil {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=none", "img", "cmd"}, nil); err != nil {
 		t.Fatalf("Unexpected error: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=host", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=host", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
 		t.Fatalf("Expected error ErrConflictNetworkHostname, got: %s", err)
 	}
 
-	if _, _, _, err := Parse([]string{"-h=name", "--net=container:other", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
+	if _, _, _, err := parseRun([]string{"-h=name", "--net=container:other", "img", "cmd"}, nil); err != ErrConflictNetworkHostname {
 		t.Fatalf("Expected error ErrConflictNetworkHostname, got: %s", err)
 	}
 }