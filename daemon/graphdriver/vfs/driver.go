50061d42a64b6ed5b819db55c94d6e20121a29cf
unclejack <unclejacksons@gmail.com>
2014-10-29 21:06:51 +0200

    add pkg/chrootarchive and use it on the daemon
    Docker-DCO-1.1-Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com> (github: unclejack)
    
    Conflicts:
    	builder/internals.go
    	daemon/graphdriver/aufs/aufs.go
    	daemon/volumes.go
    		fixed conflicts in imports

--- daemon/graphdriver/vfs/driver.go
+++ daemon/graphdriver/vfs/driver.go
@@ -8,7 +8,7 @@ import (
 	"path"
 
 	"github.com/tiborvass/docker/daemon/graphdriver"
-	"github.com/tiborvass/docker/pkg/archive"
+	"github.com/tiborvass/docker/pkg/chrootarchive"
 	"github.com/docker/libcontainer/label"
 )
 
@@ -66,7 +66,7 @@ func (d *Driver) Create(id, parent string) error {
 	if err != nil {
 		return fmt.Errorf("%s: %s", parent, err)
 	}
-	if err := archive.CopyWithTar(parentDir, dir); err != nil {
+	if err := chrootarchive.CopyWithTar(parentDir, dir); err != nil {
 		return err
 	}
 	return nil
