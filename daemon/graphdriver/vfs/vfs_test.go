aduermael/cli

don't call reexec.Init from chrootarchive
Docker-DCO-1.1-Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com> (github: unclejack)

daemon/graphdriver/vfs/vfs_test.go
@@ -1,9 +1,16 @@
 package vfs
 
 import (
-	"github.com/docker/docker/daemon/graphdriver/graphtest"
 	"testing"
+
+	"github.com/docker/docker/daemon/graphdriver/graphtest"
+
+	"github.com/docker/docker/pkg/reexec"
 )
+
+func init() {
+	reexec.Init()
+}
 
 // This avoids creating a new driver for each test if all tests are run
 // Make sure to put new tests between TestVfsSetup and TestVfsTeardown

graph/pools_test.go
@@ -1,6 +1,14 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/docker/docker/pkg/reexec"
+)
+
+func init() {
+	reexec.Init()
+}
 
 func TestPools(t *testing.T) {
 	s := &TagStore{

pkg/chrootarchive/archive_test.go
@@ -7,7 +7,12 @@ import (
 	"testing"
 
 	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/pkg/reexec"
 )
+
+func init() {
+	reexec.Init()
+}
 
 func TestChrootTarUntar(t *testing.T) {
 	tmpdir, err := ioutil.TempDir("", "docker-TestChrootTarUntar")

tiborvass/docker

Conflicts:
	daemon/graphdriver/aufs/aufs_test.go
		fixed conflict caused by imports

daemon/graphdriver/vfs/vfs_test.go
@@ -1,9 +1,16 @@
 package vfs
 
 import (
-	"github.com/tiborvass/docker/daemon/graphdriver/graphtest"
 	"testing"
+
+	"github.com/tiborvass/docker/daemon/graphdriver/graphtest"
+
+	"github.com/tiborvass/docker/pkg/reexec"
 )
+
+func init() {
+	reexec.Init()
+}
 
 // This avoids creating a new driver for each test if all tests are run
 // Make sure to put new tests between TestVfsSetup and TestVfsTeardown

graph/pools_test.go
@@ -1,6 +1,14 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/tiborvass/docker/pkg/reexec"
+)
+
+func init() {
+	reexec.Init()
+}
 
 func TestPools(t *testing.T) {
 	s := &TagStore{

pkg/chrootarchive/archive_test.go
@@ -7,7 +7,12 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/pkg/archive"
+	"github.com/tiborvass/docker/pkg/reexec"
 )
+
+func init() {
+	reexec.Init()
+}
 
 func TestChrootTarUntar(t *testing.T) {
 	tmpdir, err := ioutil.TempDir("", "docker-TestChrootTarUntar")