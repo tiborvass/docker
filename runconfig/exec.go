moby/moby

refactor redundant code around calls to cmd.Parse
Signed-off-by: Tibor Vass <teabee89@gmail.com>

runconfig/exec.go
@@ -3,7 +3,7 @@ package runconfig
 import (
 	"github.com/docker/docker/engine"
 	flag "github.com/docker/docker/pkg/mflag"
-	"os"
+	"github.com/docker/docker/utils"
 )
 
 type ExecConfig struct {
@@ -40,19 +40,12 @@ func ParseExec(cmd *flag.FlagSet, args []string) (*ExecConfig, error) {
 		flStdin   = cmd.Bool([]string{"i", "-interactive"}, false, "Keep STDIN open even if not attached")
 		flTty     = cmd.Bool([]string{"t", "-tty"}, false, "Allocate a pseudo-TTY")
 		flDetach  = cmd.Bool([]string{"d", "-detach"}, false, "Detached mode: run command in the background")
-		help      = cmd.Bool([]string{"#help", "-help"}, false, "Print usage")
 		execCmd   []string
 		container string
 	)
-	if err := cmd.Parse(args); err != nil {
+	cmd.Require(flag.Min, 2)
+	if err := utils.ParseFlags(cmd, args, true); err != nil {
 		return nil, err
-	}
-	if *help {
-		cmd.Usage()
-		return nil, nil
-	}
-	if cmd.BadArgs(flag.Min, 2) {
-		os.Exit(1)
 	}
 	container = cmd.Arg(0)
 	parsedArgs := cmd.Args()

utils/flags.go
@@ -1,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	flag "github.com/docker/docker/pkg/mflag"
+)
+
+// ParseFlags is a utility function that adds a help flag if withHelp is true,
+// calls cmd.Parse(args) and prints a relevant error message if there are incorrect number of arguments.
+// TODO: move this to a better package than utils
+func ParseFlags(cmd *flag.FlagSet, args []string, withHelp bool) error {
+	var help *bool
+	if withHelp {
+		help = cmd.Bool([]string{"#help", "-help"}, false, "Print usage")
+	}
+	if err := cmd.Parse(args); err != nil {
+		return err
+	}
+	if help != nil && *help {
+		cmd.Usage()
+		// just in case Usage does not exit
+		os.Exit(0)
+	}
+	if str := cmd.CheckArgs(); str != "" {
+		if withHelp {
+			str += ". See 'docker " + cmd.Name() + " --help'"
+		}
+		fmt.Fprintf(cmd.Out(), "docker: %s.\n", str)
+		os.Exit(1)
+	}
+	return nil
+}

tiborvass/docker

runconfig/exec.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/tiborvass/docker/engine"
 	flag "github.com/tiborvass/docker/pkg/mflag"
-	"os"
+	"github.com/tiborvass/docker/utils"
 )
 
 type ExecConfig struct {
@@ -46,19 +46,12 @@ func ParseExec(cmd *flag.FlagSet, args []string) (*ExecConfig, error) {
 		flStdin   = cmd.Bool([]string{"i", "-interactive"}, false, "Keep STDIN open even if not attached")
 		flTty     = cmd.Bool([]string{"t", "-tty"}, false, "Allocate a pseudo-TTY")
 		flDetach  = cmd.Bool([]string{"d", "-detach"}, false, "Detached mode: run command in the background")
-		help      = cmd.Bool([]string{"#help", "-help"}, false, "Print usage")
 		execCmd   []string
 		container string
 	)
-	if err := cmd.Parse(args); err != nil {
+	cmd.Require(flag.Min, 2)
+	if err := utils.ParseFlags(cmd, args, true); err != nil {
 		return nil, err
-	}
-	if *help {
-		cmd.Usage()
-		return nil, nil
-	}
-	if cmd.BadArgs(flag.Min, 2) {
-		os.Exit(1)
 	}
 	container = cmd.Arg(0)
 	parsedArgs := cmd.Args()

utils/flags.go
@@ -1,0 +1,34 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	flag "github.com/tiborvass/docker/pkg/mflag"
+)
+
+// ParseFlags is a utility function that adds a help flag if withHelp is true,
+// calls cmd.Parse(args) and prints a relevant error message if there are incorrect number of arguments.
+// TODO: move this to a better package than utils
+func ParseFlags(cmd *flag.FlagSet, args []string, withHelp bool) error {
+	var help *bool
+	if withHelp {
+		help = cmd.Bool([]string{"#help", "-help"}, false, "Print usage")
+	}
+	if err := cmd.Parse(args); err != nil {
+		return err
+	}
+	if help != nil && *help {
+		cmd.Usage()
+		// just in case Usage does not exit
+		os.Exit(0)
+	}
+	if str := cmd.CheckArgs(); str != "" {
+		if withHelp {
+			str += ". See 'docker " + cmd.Name() + " --help'"
+		}
+		fmt.Fprintf(cmd.Out(), "docker: %s.\n", str)
+		os.Exit(1)
+	}
+	return nil
+}