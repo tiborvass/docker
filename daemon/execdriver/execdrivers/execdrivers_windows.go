d66c85ee05c8b66769c16af586b1c6ccf17ed5dd
John Howard <jhoward@microsoft.com>
2015-04-27 15:20:44 -0700

    Windows: Fork execdrivers.go for Windows execdriver
    Signed-off-by: John Howard <jhoward@microsoft.com>

---
+++ daemon/execdriver/execdrivers/execdrivers_windows.go
@@ -0,0 +1,19 @@
+// +build windows
+
+package execdrivers
+
+import (
+	"fmt"
+
+	"github.com/tiborvass/docker/daemon/execdriver"
+	"github.com/tiborvass/docker/daemon/execdriver/windows"
+	"github.com/tiborvass/docker/pkg/sysinfo"
+)
+
+func NewDriver(name, root, libPath, initPath string, sysInfo *sysinfo.SysInfo) (execdriver.Driver, error) {
+	switch name {
+	case "windows":
+		return windows.NewDriver(root, initPath)
+	}
+	return nil, fmt.Errorf("unknown exec driver %s", name)
+}
