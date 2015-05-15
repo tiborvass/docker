e25d982a117591cd7b9280301fb183dcddddbacc
John Howard <jhoward@microsoft.com>
2015-05-15 15:02:11 -0700

    Windows: refactor stats
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/stats_linux.go
+++ daemon/stats_linux.go
@@ -1,60 +1,10 @@
 package daemon
 
 import (
-	"encoding/json"
-	"io"
-
-	"github.com/docker/docker/daemon/execdriver"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/libcontainer"
 	"github.com/docker/libcontainer/cgroups"
-	"github.com/docker/libnetwork/sandbox"
 )
-
-func (daemon *Daemon) ContainerStats(name string, stream bool, out io.Writer) error {
-	updates, err := daemon.SubscribeToContainerStats(name)
-	if err != nil {
-		return err
-	}
-
-	var preCpuStats types.CpuStats
-	getStat := func(v interface{}) *types.Stats {
-		update := v.(*execdriver.ResourceStats)
-		// Retrieve the nw statistics from libnetwork and inject them in the Stats
-		if nwStats, err := daemon.getNetworkStats(name); err == nil {
-			update.Stats.Interfaces = nwStats
-		}
-		ss := convertToAPITypes(update.Stats)
-		ss.PreCpuStats = preCpuStats
-		ss.MemoryStats.Limit = uint64(update.MemoryLimit)
-		ss.Read = update.Read
-		ss.CpuStats.SystemUsage = update.SystemUsage
-		preCpuStats = ss.CpuStats
-		return ss
-	}
-
-	enc := json.NewEncoder(out)
-
-	if !stream {
-		// prime the cpu stats so they aren't 0 in the final output
-		s := getStat(<-updates)
-
-		// now pull stats again with the cpu stats primed
-		s = getStat(<-updates)
-		err := enc.Encode(s)
-		daemon.UnsubscribeToContainerStats(name, updates)
-		return err
-	}
-
-	for v := range updates {
-		s := getStat(v)
-		if err := enc.Encode(s); err != nil {
-			// TODO: handle the specific broken pipe
-			daemon.UnsubscribeToContainerStats(name, updates)
-			return err
-		}
-	}
-	return nil
-}
 
 // convertToAPITypes converts the libcontainer.Stats to the api specific
 // structs.  This is done to preserve API compatibility and versioning.
@@ -73,6 +23,7 @@ func convertToAPITypes(ls *libcontainer.Stats) *types.Stats {
 			s.Network.TxDropped += iface.TxDropped
 		}
 	}
+
 	cs := ls.CgroupStats
 	if cs != nil {
 		s.BlkioStats = types.BlkioStats{
@@ -107,6 +58,7 @@ func convertToAPITypes(ls *libcontainer.Stats) *types.Stats {
 			Failcnt:  mem.Failcnt,
 		}
 	}
+
 	return s
 }
 
@@ -121,47 +73,4 @@ func copyBlkioEntry(entries []cgroups.BlkioStatEntry) []types.BlkioStatEntry {
 		}
 	}
 	return out
-}
-
-func (daemon *Daemon) getNetworkStats(name string) ([]*libcontainer.NetworkInterface, error) {
-	var list []*libcontainer.NetworkInterface
-
-	c, err := daemon.Get(name)
-	if err != nil {
-		return list, err
-	}
-
-	nw, err := daemon.netController.NetworkByID(c.NetworkSettings.NetworkID)
-	if err != nil {
-		return list, err
-	}
-	ep, err := nw.EndpointByID(c.NetworkSettings.EndpointID)
-	if err != nil {
-		return list, err
-	}
-
-	stats, err := ep.Statistics()
-	if err != nil {
-		return list, err
-	}
-
-	// Convert libnetwork nw stats into libcontainer nw stats
-	for ifName, ifStats := range stats {
-		list = append(list, convertLnNetworkStats(ifName, ifStats))
-	}
-
-	return list, nil
-}
-
-func convertLnNetworkStats(name string, stats *sandbox.InterfaceStatistics) *libcontainer.NetworkInterface {
-	n := &libcontainer.NetworkInterface{Name: name}
-	n.RxBytes = stats.RxBytes
-	n.RxPackets = stats.RxPackets
-	n.RxErrors = stats.RxErrors
-	n.RxDropped = stats.RxDropped
-	n.TxBytes = stats.TxBytes
-	n.TxPackets = stats.TxPackets
-	n.TxErrors = stats.TxErrors
-	n.TxDropped = stats.TxDropped
-	return n
 }
