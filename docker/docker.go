a6d1351708cfbead0886461035aabeef4ce55c07
Arnaud Porterie <arnaud.porterie@docker.com>
2015-05-19 15:09:58 -0700

    Add DOCKER_EXPERIMENTAL environment variable
    The DOCKER_EXPERIMENTAL environment variable drives the activation of
    the 'experimental' build tag.
    
    Signed-off-by: Arnaud Porterie <arnaud.porterie@docker.com>

--- docker/docker.go
+++ docker/docker.go
@@ -16,6 +16,7 @@ import (
 	flag "github.com/tiborvass/docker/pkg/mflag"
 	"github.com/tiborvass/docker/pkg/reexec"
 	"github.com/tiborvass/docker/pkg/term"
+	"github.com/tiborvass/docker/utils"
 )
 
 const (
@@ -57,6 +58,10 @@ func main() {
 	if *flDebug {
 		os.Setenv("DEBUG", "1")
 		setLogLevel(logrus.DebugLevel)
+	}
+
+	if utils.ExperimentalBuild() {
+		logrus.Warn("Running experimental build")
 	}
 
 	if len(flHosts) == 0 {
