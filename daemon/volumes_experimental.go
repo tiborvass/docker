5e88c947864ff4ca70c72a3603dcf3151d53afb0
David Calavera <david.calavera@gmail.com>
2015-05-21 10:57:59 -0700

    Allow named volumes for external drivers.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- daemon/volumes_experimental.go
+++ daemon/volumes_experimental.go
@@ -3,6 +3,9 @@
 package daemon
 
 import (
+	"path/filepath"
+
+	"github.com/tiborvass/docker/runconfig"
 	"github.com/tiborvass/docker/volume"
 	"github.com/tiborvass/docker/volume/drivers"
 )
@@ -12,4 +15,12 @@ func getVolumeDriver(name string) (volume.Driver, error) {
 		name = volume.DefaultDriverName
 	}
 	return volumedrivers.Lookup(name)
+}
+
+func parseVolumeSource(spec string, config *runconfig.Config) (string, string, error) {
+	if !filepath.IsAbs(spec) {
+		return spec, "", nil
+	}
+
+	return "", spec, nil
 }
--- daemon/volumes_stubs.go
+++ daemon/volumes_stubs.go
@@ -3,10 +3,22 @@
 package daemon
 
 import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/tiborvass/docker/runconfig"
 	"github.com/tiborvass/docker/volume"
 	"github.com/tiborvass/docker/volume/drivers"
 )
 
 func getVolumeDriver(_ string) (volume.Driver, error) {
 	return volumedrivers.Lookup(volume.DefaultDriverName)
+}
+
+func parseVolumeSource(spec string, _ *runconfig.Config) (string, string, error) {
+	if !filepath.IsAbs(spec) {
+		return "", "", fmt.Errorf("cannot bind mount volume: %s volume paths must be absolute.", spec)
+	}
+
+	return "", spec, nil
 }
