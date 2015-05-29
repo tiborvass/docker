b75020d554116b50036d5be6be22ca2337d30000
Marius Sturm <marius@graylog.com>
2015-05-29 23:00:46 +0200

    add UDP GELF logging-driver
    allows to send container logs to Graylog or Logstash.
    
    Signed-off-by: Marius Sturm <marius@graylog.com>

--- daemon/logdrivers_linux.go
+++ daemon/logdrivers_linux.go
@@ -3,6 +3,7 @@ package daemon
 // Importing packages here only to make sure their init gets called and
 // therefore they register themselves to the logdriver factory.
 import (
+	_ "github.com/tiborvass/docker/daemon/logger/gelf"
 	_ "github.com/tiborvass/docker/daemon/logger/journald"
 	_ "github.com/tiborvass/docker/daemon/logger/jsonfilelog"
 	_ "github.com/tiborvass/docker/daemon/logger/syslog"
