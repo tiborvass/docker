0fa775d31d5d6cdef5f38d0943594f23330f9160
Tibor Vass <tibor@docker.com>
2015-06-08 19:56:37 -0400

    Do not set auth headers if 302
    This patch ensures no auth headers are set for v1 registries if there
    was a 302 redirect.
    
    This also ensures v2 does not use authTransport.
    
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- registry/session.go
+++ registry/session.go
@@ -93,13 +93,7 @@ func (tr *authTransport) RoundTrip(orig *http.Request) (*http.Response, error) {
 	if req.Header.Get("Authorization") == "" {
 		if req.Header.Get("X-Docker-Token") == "true" && len(tr.Username) > 0 {
 			req.SetBasicAuth(tr.Username, tr.Password)
-		} else if len(tr.token) > 0 &&
-			// Authorization should not be set on 302 redirect for untrusted locations.
-			// This logic mirrors the behavior in AddRequiredHeadersToRedirectedRequests.
-			// As the authorization logic is currently implemented in RoundTrip,
-			// a 302 redirect is detected by looking at the Referer header as go http package adds said header.
-			// This is safe as Docker doesn't set Referer in other scenarios.
-			(req.Header.Get("Referer") == "" || trustedLocation(orig)) {
+		} else if len(tr.token) > 0 {
 			req.Header.Set("Authorization", "Token "+strings.Join(tr.token, ","))
 		}
 	}
