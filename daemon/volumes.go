container-os/cli

Allow to downgrade local volumes from > 1.7 to 1.6.
Signed-off-by: David Calavera <david.calavera@gmail.com>
Upstream-commit: bd9814f0db9c8a087e42b66eabb8413bf1b2ab66
Component: engine

daemon/volumes.go
@@ -1,16 +1,17 @@
 package daemon
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/pkg/chrootarchive"
 	"github.com/docker/docker/runconfig"
 	"github.com/docker/docker/volume"
+	"github.com/docker/docker/volume/local"
 	"github.com/docker/libcontainer/label"
 )
 
@@ -50,6 +51,13 @@ func (m *mountPoint) Path() string {
 	}
 
 	return m.Source
+}
+
+// BackwardsCompatible decides whether this mount point can be
+// used in old versions of Docker or not.
+// Only bind mounts and local volumes can be used in old versions of Docker.
+func (m *mountPoint) BackwardsCompatible() bool {
+	return len(m.Source) > 0 || m.Driver == volume.DefaultDriverName
 }
 
 func parseBindMount(spec string, mountLabel string, config *runconfig.Config) (*mountPoint, error) {
@@ -231,8 +239,20 @@ func (daemon *Daemon) registerMountPoints(container *Container, hostConfig *runc
 		mountPoints[bind.Destination] = bind
 	}
 
+	// Keep backwards compatible structures
+	bcVolumes := map[string]string{}
+	bcVolumesRW := map[string]bool{}
+	for _, m := range mountPoints {
+		if m.BackwardsCompatible() {
+			bcVolumes[m.Destination] = m.Path()
+			bcVolumesRW[m.Destination] = m.RW
+		}
+	}
+
 	container.Lock()
 	container.MountPoints = mountPoints
+	container.Volumes = bcVolumes
+	container.VolumesRW = bcVolumesRW
 	container.Unlock()
 
 	return nil
@@ -241,53 +261,77 @@ func (daemon *Daemon) registerMountPoints(container *Container, hostConfig *runc
 // verifyVolumesInfo ports volumes configured for the containers pre docker 1.7.
 // It reads the container configuration and creates valid mount points for the old volumes.
 func (daemon *Daemon) verifyVolumesInfo(container *Container) error {
-	jsonPath, err := container.jsonPath()
-	if err != nil {
-		return err
-	}
-	f, err := os.Open(jsonPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return err
-	}
+	// Inspect old structures only when we're upgrading from old versions
+	// to versions >= 1.7 and the MountPoints has not been populated with volumes data.
+	if len(container.MountPoints) == 0 && len(container.Volumes) > 0 {
+		for destination, hostPath := range container.Volumes {
+			vfsPath := filepath.Join(daemon.root, "vfs", "dir")
+			rw := container.VolumesRW != nil && container.VolumesRW[destination]
 
-	type oldContVolCfg struct {
-		Volumes   map[string]string
-		VolumesRW map[string]bool
-	}
-
-	vols := oldContVolCfg{
-		Volumes:   make(map[string]string),
-		VolumesRW: make(map[string]bool),
-	}
-	if err := json.NewDecoder(f).Decode(&vols); err != nil {
-		return err
-	}
-
-	for destination, hostPath := range vols.Volumes {
-		vfsPath := filepath.Join(daemon.root, "vfs", "dir")
-		rw := vols.VolumesRW[destination]
-
-		if strings.HasPrefix(hostPath, vfsPath) {
-			id := filepath.Base(hostPath)
-			if err := daemon.migrateVolume(id, hostPath); err != nil {
-				return err
+			if strings.HasPrefix(hostPath, vfsPath) {
+				id := filepath.Base(hostPath)
+				if err := migrateVolume(id, hostPath); err != nil {
+					return err
+				}
+				container.addLocalMountPoint(id, destination, rw)
+			} else { // Bind mount
+				id, source, err := parseVolumeSource(hostPath)
+				// We should not find an error here coming
+				// from the old configuration, but who knows.
+				if err != nil {
+					return err
+				}
+				container.addBindMountPoint(id, source, destination, rw)
 			}
-			container.addLocalMountPoint(id, destination, rw)
-		} else { // Bind mount
-			id, source, err := parseVolumeSource(hostPath)
-			// We should not find an error here coming
-			// from the old configuration, but who knows.
+		}
+	} else if len(container.MountPoints) > 0 {
+		// Volumes created with a Docker version >= 1.7. We verify integrity in case of data created
+		// with Docker 1.7 RC versions that put the information in
+		// DOCKER_ROOT/volumes/VOLUME_ID rather than DOCKER_ROOT/volumes/VOLUME_ID/_container_data.
+		l, err := getVolumeDriver(volume.DefaultDriverName)
+		if err != nil {
+			return err
+		}
+
+		for _, m := range container.MountPoints {
+			if m.Driver != volume.DefaultDriverName {
+				continue
+			}
+			dataPath := l.(*local.Root).DataPath(m.Name)
+			volumePath := filepath.Dir(dataPath)
+
+			d, err := ioutil.ReadDir(volumePath)
 			if err != nil {
+				// If the volume directory doesn't exist yet it will be recreated,
+				// so we only return the error when there is a different issue.
+				if !os.IsNotExist(err) {
+					return err
+				}
+				// Do not check when the volume directory does not exist.
+				continue
+			}
+			if validVolumeLayout(d) {
+				continue
+			}
+
+			if err := os.Mkdir(dataPath, 0755); err != nil {
 				return err
 			}
-			container.addBindMountPoint(id, source, destination, rw)
+
+			// Move data inside the data directory
+			for _, f := range d {
+				oldp := filepath.Join(volumePath, f.Name())
+				newp := filepath.Join(dataPath, f.Name())
+				if err := os.Rename(oldp, newp); err != nil {
+					logrus.Errorf("Unable to move %s to %s\n", oldp, newp)
+				}
+			}
 		}
+
+		return container.ToDisk()
 	}
 
-	return container.ToDisk()
+	return nil
 }
 
 func createVolume(name, driverName string) (volume.Volume, error) {

daemon/volumes_linux.go
@@ -8,9 +8,10 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/daemon/execdriver"
 	"github.com/docker/docker/pkg/system"
+	"github.com/docker/docker/volume"
+	"github.com/docker/docker/volume/local"
 )
 
 // copyOwnership copies the permissions and uid:gid of the source file
@@ -70,33 +71,49 @@ func (m mounts) parts(i int) int {
 	return len(strings.Split(filepath.Clean(m[i].Destination), string(os.PathSeparator)))
 }
 
-// migrateVolume moves the contents of a volume created pre Docker 1.7
-// to the location expected by the local driver. Steps:
-// 1. Save old directory that includes old volume's config json file.
-// 2. Move virtual directory with content to where the local driver expects it to be.
-// 3. Remove the backup of the old volume config.
-func (daemon *Daemon) migrateVolume(id, vfs string) error {
-	volumeInfo := filepath.Join(daemon.root, defaultVolumesPathName, id)
-	backup := filepath.Join(daemon.root, defaultVolumesPathName, id+".back")
-
-	var err error
-	if err = os.Rename(volumeInfo, backup); err != nil {
+// migrateVolume links the contents of a volume created pre Docker 1.7
+// into the location expected by the local driver.
+// It creates a symlink from DOCKER_ROOT/vfs/dir/VOLUME_ID to DOCKER_ROOT/volumes/VOLUME_ID/_container_data.
+// It preserves the volume json configuration generated pre Docker 1.7 to be able to
+// downgrade from Docker 1.7 to Docker 1.6 without losing volume compatibility.
+func migrateVolume(id, vfs string) error {
+	l, err := getVolumeDriver(volume.DefaultDriverName)
+	if err != nil {
 		return err
 	}
-	defer func() {
-		// Put old configuration back in place in case one of the next steps fails.
-		if err != nil {
-			os.Rename(backup, volumeInfo)
+
+	newDataPath := l.(*local.Root).DataPath(id)
+	fi, err := os.Stat(newDataPath)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	if fi != nil && fi.IsDir() {
+		return nil
+	}
+
+	return os.Symlink(vfs, newDataPath)
+}
+
+// validVolumeLayout checks whether the volume directory layout
+// is valid to work with Docker post 1.7 or not.
+func validVolumeLayout(files []os.FileInfo) bool {
+	if len(files) == 1 && files[0].Name() == local.VolumeDataPathName && files[0].IsDir() {
+		return true
+	}
+
+	if len(files) != 2 {
+		return false
+	}
+
+	for _, f := range files {
+		if f.Name() == "config.json" ||
+			(f.Name() == local.VolumeDataPathName && f.Mode()&os.ModeSymlink == os.ModeSymlink) {
+			// Old volume configuration, we ignore it
+			continue
 		}
-	}()
-
-	if err = os.Rename(vfs, volumeInfo); err != nil {
-		return err
+		return false
 	}
 
-	if err = os.RemoveAll(backup); err != nil {
-		logrus.Errorf("Unable to remove volume info backup directory %s: %v", backup, err)
-	}
-
-	return nil
+	return true
 }

daemon/volumes_windows.go
@@ -2,7 +2,11 @@
 
 package daemon
 
-import "github.com/docker/docker/daemon/execdriver"
+import (
+	"os"
+
+	"github.com/docker/docker/daemon/execdriver"
+)
 
 // Not supported on Windows
 func copyOwnership(source, destination string) error {
@@ -13,6 +17,10 @@ func (container *Container) setupMounts() ([]execdriver.Mount, error) {
 	return nil, nil
 }
 
-func (daemon *Daemon) migrateVolume(id, vfs string) error {
+func migrateVolume(id, vfs string) error {
 	return nil
+}
+
+func validVolumeLayout(files []os.FileInfo) bool {
+	return true
 }

tiborvass/docker

(cherry picked from commit bd9814f0db9c8a087e42b66eabb8413bf1b2ab66)

daemon/volumes.go
@@ -1,16 +1,17 @@
 package daemon
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/tiborvass/docker/pkg/chrootarchive"
 	"github.com/tiborvass/docker/runconfig"
 	"github.com/tiborvass/docker/volume"
+	"github.com/tiborvass/docker/volume/local"
 	"github.com/docker/libcontainer/label"
 )
 
@@ -50,6 +51,13 @@ func (m *mountPoint) Path() string {
 	}
 
 	return m.Source
+}
+
+// BackwardsCompatible decides whether this mount point can be
+// used in old versions of Docker or not.
+// Only bind mounts and local volumes can be used in old versions of Docker.
+func (m *mountPoint) BackwardsCompatible() bool {
+	return len(m.Source) > 0 || m.Driver == volume.DefaultDriverName
 }
 
 func parseBindMount(spec string, mountLabel string, config *runconfig.Config) (*mountPoint, error) {
@@ -231,8 +239,20 @@ func (daemon *Daemon) registerMountPoints(container *Container, hostConfig *runc
 		mountPoints[bind.Destination] = bind
 	}
 
+	// Keep backwards compatible structures
+	bcVolumes := map[string]string{}
+	bcVolumesRW := map[string]bool{}
+	for _, m := range mountPoints {
+		if m.BackwardsCompatible() {
+			bcVolumes[m.Destination] = m.Path()
+			bcVolumesRW[m.Destination] = m.RW
+		}
+	}
+
 	container.Lock()
 	container.MountPoints = mountPoints
+	container.Volumes = bcVolumes
+	container.VolumesRW = bcVolumesRW
 	container.Unlock()
 
 	return nil
@@ -241,53 +261,77 @@ func (daemon *Daemon) registerMountPoints(container *Container, hostConfig *runc
 // verifyVolumesInfo ports volumes configured for the containers pre docker 1.7.
 // It reads the container configuration and creates valid mount points for the old volumes.
 func (daemon *Daemon) verifyVolumesInfo(container *Container) error {
-	jsonPath, err := container.jsonPath()
-	if err != nil {
-		return err
-	}
-	f, err := os.Open(jsonPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return err
-	}
+	// Inspect old structures only when we're upgrading from old versions
+	// to versions >= 1.7 and the MountPoints has not been populated with volumes data.
+	if len(container.MountPoints) == 0 && len(container.Volumes) > 0 {
+		for destination, hostPath := range container.Volumes {
+			vfsPath := filepath.Join(daemon.root, "vfs", "dir")
+			rw := container.VolumesRW != nil && container.VolumesRW[destination]
 
-	type oldContVolCfg struct {
-		Volumes   map[string]string
-		VolumesRW map[string]bool
-	}
-
-	vols := oldContVolCfg{
-		Volumes:   make(map[string]string),
-		VolumesRW: make(map[string]bool),
-	}
-	if err := json.NewDecoder(f).Decode(&vols); err != nil {
-		return err
-	}
-
-	for destination, hostPath := range vols.Volumes {
-		vfsPath := filepath.Join(daemon.root, "vfs", "dir")
-		rw := vols.VolumesRW[destination]
-
-		if strings.HasPrefix(hostPath, vfsPath) {
-			id := filepath.Base(hostPath)
-			if err := daemon.migrateVolume(id, hostPath); err != nil {
-				return err
+			if strings.HasPrefix(hostPath, vfsPath) {
+				id := filepath.Base(hostPath)
+				if err := migrateVolume(id, hostPath); err != nil {
+					return err
+				}
+				container.addLocalMountPoint(id, destination, rw)
+			} else { // Bind mount
+				id, source, err := parseVolumeSource(hostPath)
+				// We should not find an error here coming
+				// from the old configuration, but who knows.
+				if err != nil {
+					return err
+				}
+				container.addBindMountPoint(id, source, destination, rw)
 			}
-			container.addLocalMountPoint(id, destination, rw)
-		} else { // Bind mount
-			id, source, err := parseVolumeSource(hostPath)
-			// We should not find an error here coming
-			// from the old configuration, but who knows.
+		}
+	} else if len(container.MountPoints) > 0 {
+		// Volumes created with a Docker version >= 1.7. We verify integrity in case of data created
+		// with Docker 1.7 RC versions that put the information in
+		// DOCKER_ROOT/volumes/VOLUME_ID rather than DOCKER_ROOT/volumes/VOLUME_ID/_container_data.
+		l, err := getVolumeDriver(volume.DefaultDriverName)
+		if err != nil {
+			return err
+		}
+
+		for _, m := range container.MountPoints {
+			if m.Driver != volume.DefaultDriverName {
+				continue
+			}
+			dataPath := l.(*local.Root).DataPath(m.Name)
+			volumePath := filepath.Dir(dataPath)
+
+			d, err := ioutil.ReadDir(volumePath)
 			if err != nil {
+				// If the volume directory doesn't exist yet it will be recreated,
+				// so we only return the error when there is a different issue.
+				if !os.IsNotExist(err) {
+					return err
+				}
+				// Do not check when the volume directory does not exist.
+				continue
+			}
+			if validVolumeLayout(d) {
+				continue
+			}
+
+			if err := os.Mkdir(dataPath, 0755); err != nil {
 				return err
 			}
-			container.addBindMountPoint(id, source, destination, rw)
+
+			// Move data inside the data directory
+			for _, f := range d {
+				oldp := filepath.Join(volumePath, f.Name())
+				newp := filepath.Join(dataPath, f.Name())
+				if err := os.Rename(oldp, newp); err != nil {
+					logrus.Errorf("Unable to move %s to %s\n", oldp, newp)
+				}
+			}
 		}
+
+		return container.ToDisk()
 	}
 
-	return container.ToDisk()
+	return nil
 }
 
 func createVolume(name, driverName string) (volume.Volume, error) {

daemon/volumes_linux.go
@@ -8,9 +8,10 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/tiborvass/docker/daemon/execdriver"
 	"github.com/tiborvass/docker/pkg/system"
+	"github.com/tiborvass/docker/volume"
+	"github.com/tiborvass/docker/volume/local"
 )
 
 // copyOwnership copies the permissions and uid:gid of the source file
@@ -70,33 +71,49 @@ func (m mounts) parts(i int) int {
 	return len(strings.Split(filepath.Clean(m[i].Destination), string(os.PathSeparator)))
 }
 
-// migrateVolume moves the contents of a volume created pre Docker 1.7
-// to the location expected by the local driver. Steps:
-// 1. Save old directory that includes old volume's config json file.
-// 2. Move virtual directory with content to where the local driver expects it to be.
-// 3. Remove the backup of the old volume config.
-func (daemon *Daemon) migrateVolume(id, vfs string) error {
-	volumeInfo := filepath.Join(daemon.root, defaultVolumesPathName, id)
-	backup := filepath.Join(daemon.root, defaultVolumesPathName, id+".back")
-
-	var err error
-	if err = os.Rename(volumeInfo, backup); err != nil {
+// migrateVolume links the contents of a volume created pre Docker 1.7
+// into the location expected by the local driver.
+// It creates a symlink from DOCKER_ROOT/vfs/dir/VOLUME_ID to DOCKER_ROOT/volumes/VOLUME_ID/_container_data.
+// It preserves the volume json configuration generated pre Docker 1.7 to be able to
+// downgrade from Docker 1.7 to Docker 1.6 without losing volume compatibility.
+func migrateVolume(id, vfs string) error {
+	l, err := getVolumeDriver(volume.DefaultDriverName)
+	if err != nil {
 		return err
 	}
-	defer func() {
-		// Put old configuration back in place in case one of the next steps fails.
-		if err != nil {
-			os.Rename(backup, volumeInfo)
+
+	newDataPath := l.(*local.Root).DataPath(id)
+	fi, err := os.Stat(newDataPath)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+
+	if fi != nil && fi.IsDir() {
+		return nil
+	}
+
+	return os.Symlink(vfs, newDataPath)
+}
+
+// validVolumeLayout checks whether the volume directory layout
+// is valid to work with Docker post 1.7 or not.
+func validVolumeLayout(files []os.FileInfo) bool {
+	if len(files) == 1 && files[0].Name() == local.VolumeDataPathName && files[0].IsDir() {
+		return true
+	}
+
+	if len(files) != 2 {
+		return false
+	}
+
+	for _, f := range files {
+		if f.Name() == "config.json" ||
+			(f.Name() == local.VolumeDataPathName && f.Mode()&os.ModeSymlink == os.ModeSymlink) {
+			// Old volume configuration, we ignore it
+			continue
 		}
-	}()
-
-	if err = os.Rename(vfs, volumeInfo); err != nil {
-		return err
+		return false
 	}
 
-	if err = os.RemoveAll(backup); err != nil {
-		logrus.Errorf("Unable to remove volume info backup directory %s: %v", backup, err)
-	}
-
-	return nil
+	return true
 }

daemon/volumes_windows.go
@@ -2,7 +2,11 @@
 
 package daemon
 
-import "github.com/tiborvass/docker/daemon/execdriver"
+import (
+	"os"
+
+	"github.com/tiborvass/docker/daemon/execdriver"
+)
 
 // Not supported on Windows
 func copyOwnership(source, destination string) error {
@@ -13,6 +17,10 @@ func (container *Container) setupMounts() ([]execdriver.Mount, error) {
 	return nil, nil
 }
 
-func (daemon *Daemon) migrateVolume(id, vfs string) error {
+func migrateVolume(id, vfs string) error {
 	return nil
+}
+
+func validVolumeLayout(files []os.FileInfo) bool {
+	return true
 }