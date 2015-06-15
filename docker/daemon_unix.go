1eb973550a28ffc4fda32407394db8623f90af5c
unclejack <unclejacksons@gmail.com>
2015-06-15 16:36:19 +0300

    docker/daemon: set umask to the default on startup
    This sets up the umask so that it's the same on all systems.
    
    Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com>

--- docker/daemon_unix.go
+++ docker/daemon_unix.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"syscall"
 
 	apiserver "github.com/tiborvass/docker/api/server"
 	"github.com/tiborvass/docker/daemon"
@@ -27,4 +29,16 @@ func currentUserIsOwner(f string) bool {
 		}
 	}
 	return false
+}
+
+// setDefaultUmask sets the umask to 0022 to avoid problems
+// caused by custom umask
+func setDefaultUmask() error {
+	desiredUmask := 0022
+	syscall.Umask(desiredUmask)
+	if umask := syscall.Umask(desiredUmask); umask != desiredUmask {
+		return fmt.Errorf("failed to set umask: expected %#o, got %#o", desiredUmask, umask)
+	}
+
+	return nil
 }
