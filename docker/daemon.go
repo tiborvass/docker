dc9144fd82856c59b1d5e4367f4bf87e1579bc2b
wlan0 <sidharthamn@gmail.com>
2015-06-30 17:40:13 -0700

    add support for maximum log size, and max number of log files
    Signed-off-by: wlan0 <sidharthamn@gmail.com>

--- docker/daemon.go
+++ docker/daemon.go
@@ -15,6 +15,7 @@ import (
 	"github.com/tiborvass/docker/autogen/dockerversion"
 	"github.com/tiborvass/docker/cliconfig"
 	"github.com/tiborvass/docker/daemon"
+	"github.com/tiborvass/docker/daemon/logger"
 	flag "github.com/tiborvass/docker/pkg/mflag"
 	"github.com/tiborvass/docker/pkg/pidfile"
 	"github.com/tiborvass/docker/pkg/signal"
@@ -92,6 +93,12 @@ func mainDaemon() {
 
 	if err := setDefaultUmask(); err != nil {
 		logrus.Fatalf("Failed to set umask: %v", err)
+	}
+
+	if len(daemonCfg.LogConfig.Config) > 0 {
+		if err := logger.ValidateLogOpts(daemonCfg.LogConfig.Type, daemonCfg.LogConfig.Config); err != nil {
+			logrus.Fatalf("Failed to set log opts: %v", err)
+		}
 	}
 
 	var pfile *pidfile.PidFile
