b934106e3547ce7efdc5b119b809041a7e5a4dd8
Tibor Vass <tibor@docker.com>
2015-07-30 21:19:51 -0400

    Use default no-op uuid.Loggerf for client cli
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- api/client/cli.go
+++ api/client/cli.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/docker/distribution/uuid"
 	"github.com/tiborvass/docker/cli"
 	"github.com/tiborvass/docker/cliconfig"
 	"github.com/tiborvass/docker/opts"
@@ -99,9 +98,6 @@ func NewDockerCli(in io.ReadCloser, out, err io.Writer, clientFlags *cli.ClientF
 	}
 
 	cli.init = func() error {
-
-		// ignore errors from uuid package when running client commands
-		uuid.Loggerf = func(string, ...interface{}) {}
 
 		clientFlags.PostParse()
 
