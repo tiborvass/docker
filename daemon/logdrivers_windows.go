be60f833dbdfcf6ee04e5e1da8e4e252bf93a973
Samuel Karp <skarp@amazon.com>
2015-08-05 00:35:06 +0000

    Add awslogs driver for Amazon CloudWatch Logs
    Signed-off-by: Samuel Karp <skarp@amazon.com>

--- daemon/logdrivers_windows.go
+++ daemon/logdrivers_windows.go
@@ -3,5 +3,6 @@ package daemon
 import (
 	// Importing packages here only to make sure their init gets called and
 	// therefore they register themselves to the logdriver factory.
+	_ "github.com/tiborvass/docker/daemon/logger/awslogs"
 	_ "github.com/tiborvass/docker/daemon/logger/jsonfilelog"
 )
