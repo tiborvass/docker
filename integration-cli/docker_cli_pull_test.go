220c03c31d77002c50bddf1e3b5680cb813c7b88
Vincent Demeester <vincent@sbr.pm>
2015-09-09 15:36:44 +0200

    Add unit tests for integration cli utils function
    - utils_test.go and docker_utils_test.go
    - Moved docker related function to docker_utils.go
    - add a test for integration-cli/checker
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- integration-cli/docker_cli_pull_test.go
+++ integration-cli/docker_cli_pull_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/docker/distribution/digest"
-	"github.com/tiborvass/docker/integration-cli/checker"
+	"github.com/tiborvass/docker/pkg/integration/checker"
 	"github.com/go-check/check"
 )
 
