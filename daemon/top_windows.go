0f7b63d873d1c4642edb9a96636be686dc4e8343
Doug Davis <dug@us.ibm.com>
2015-09-10 15:01:18 -0700

    Add context.RequestID to event stream
    This PR adds a "request ID" to each event generated, the 'docker events'
    stream now looks like this:
    
    ```
    2015-09-10T15:02:50.000000000-07:00 [reqid: c01e3534ddca] de7c5d4ca927253cf4e978ee9c4545161e406e9b5a14617efb52c658b249174a: (from ubuntu) create
    ```
    Note the `[reqID: c01e3534ddca]` part, that's new.
    
    Each HTTP request will generate its own unique ID. So, if you do a
    `docker build` you'll see a series of events all with the same reqID.
    This allow for log processing tools to determine which events are all related
    to the same http request.
    
    I didn't propigate the context to all possible funcs in the daemon,
    I decided to just do the ones that needed it in order to get the reqID
    into the events. I'd like to have people review this direction first, and
    if we're ok with it then I'll make sure we're consistent about when
    we pass around the context - IOW, make sure that all funcs at the same level
    have a context passed in even if they don't call the log funcs - this will
    ensure we're consistent w/o passing it around for all calls unnecessarily.
    
    ping @icecrime @calavera @crosbymichael
    
    Signed-off-by: Doug Davis <dug@us.ibm.com>

--- daemon/top_windows.go
+++ daemon/top_windows.go
@@ -2,10 +2,11 @@ package daemon
 
 import (
 	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/context"
 	derr "github.com/tiborvass/docker/errors"
 )
 
 // ContainerTop is not supported on Windows and returns an error.
-func (daemon *Daemon) ContainerTop(name string, psArgs string) (*types.ContainerProcessList, error) {
+func (daemon *Daemon) ContainerTop(ctx context.Context, name string, psArgs string) (*types.ContainerProcessList, error) {
 	return nil, derr.ErrorCodeNoTop
 }
