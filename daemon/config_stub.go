e54fb0243751ef572d90b1c07828f7a9ad11f020
Madhu Venugopal <madhu@docker.com>
2015-09-25 03:19:17 -0700

    Network remote APIs using new router, --net=<user-defined-network> changes
    * Moving Network Remote APIs out of experimental
    * --net can now accept user created networks using network drivers/plugins
    * Removed the experimental services concept and --default-network option
    * Neccessary backend changes to accomodate multiple networks per container
    * Integration Tests
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>
    Signed-off-by: Madhu Venugopal <madhu@docker.com>

--- daemon/config_stub.go
+++
@@ -1,8 +0,0 @@
-// +build !experimental
-
-package daemon
-
-import flag "github.com/tiborvass/docker/pkg/mflag"
-
-func (config *Config) attachExperimentalFlags(cmd *flag.FlagSet, usageFn func(string) string) {
-}
