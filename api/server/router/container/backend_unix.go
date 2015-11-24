moby/moby

Add container package for container APIs.
Signed-off-by: Dong Chen <dongluo.chen@docker.com>

api/server/router/container/backend_unix.go
@@ -1,0 +1,56 @@
+// +build !windows
+
+package container
+
+import (
+	"io"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/versions/v1p19"
+	"github.com/docker/docker/api/types/versions/v1p20"
+	"github.com/docker/docker/daemon"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/runconfig"
+)
+
+// Backend is all the methods that need to be implemented to provide
+// container specific functionality
+type Backend interface {
+	ContainerArchivePath(name string, path string) (content io.ReadCloser, stat *types.ContainerPathStat, err error)
+	ContainerAttachWithLogs(prefixOrName string, c *daemon.ContainerAttachWithLogsConfig) error
+	ContainerChanges(name string) ([]archive.Change, error)
+	ContainerCopy(name string, res string) (io.ReadCloser, error)
+	ContainerCreate(params *daemon.ContainerCreateConfig) (types.ContainerCreateResponse, error)
+	ContainerExecCreate(config *runconfig.ExecConfig) (string, error)
+	ContainerExecInspect(id string) (*daemon.ExecConfig, error)
+	ContainerExecResize(name string, height, width int) error
+	ContainerExecStart(name string, stdin io.ReadCloser, stdout io.Writer, stderr io.Writer) error
+	ContainerExport(name string, out io.Writer) error
+	ContainerExtractToDir(name, path string, noOverwriteDirNonDir bool, content io.Reader) error
+	ContainerInspect(name string, size bool) (*types.ContainerJSON, error)
+	ContainerInspect120(name string) (*v1p20.ContainerJSON, error)
+	// unix version
+	ContainerInspectPre120(name string) (*v1p19.ContainerJSON, error)
+	// windows version
+	//ContainerInspectPre120(name string) (*types.ContainerJSON, error)
+	ContainerKill(name string, sig uint64) error
+	ContainerLogs(containerName string, config *daemon.ContainerLogsConfig) error
+	ContainerPause(name string) error
+	ContainerRename(oldName, newName string) error
+	ContainerResize(name string, height, width int) error
+	ContainerRestart(name string, seconds int) error
+	ContainerRm(name string, config *daemon.ContainerRmConfig) error
+	Containers(config *daemon.ContainersConfig) ([]*types.Container, error)
+	ContainerStart(name string, hostConfig *runconfig.HostConfig) error
+	ContainerStatPath(name string, path string) (stat *types.ContainerPathStat, err error)
+	ContainerStats(prefixOrName string, config *daemon.ContainerStatsConfig) error
+	ContainerStop(name string, seconds int) error
+	ContainerTop(name string, psArgs string) (*types.ContainerProcessList, error)
+	ContainerUnpause(name string) error
+	ContainerWait(name string, timeout time.Duration) (int, error)
+	ContainerWsAttachWithLogs(prefixOrName string, c *daemon.ContainerWsAttachWithLogsConfig) error
+	ExecExists(name string) (bool, error)
+	Exists(id string) bool
+	IsPaused(id string) bool
+}

api/server/router/container/backend_windows.go
@@ -1,0 +1,55 @@
+// +build windows
+
+package container
+
+import (
+	"io"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/versions/v1p20"
+	"github.com/docker/docker/daemon"
+	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/runconfig"
+)
+
+// Backend is all the methods that need to be implemented to provide
+// container specific functionality
+type Backend interface {
+	ContainerArchivePath(name string, path string) (content io.ReadCloser, stat *types.ContainerPathStat, err error)
+	ContainerAttachWithLogs(prefixOrName string, c *daemon.ContainerAttachWithLogsConfig) error
+	ContainerChanges(name string) ([]archive.Change, error)
+	ContainerCopy(name string, res string) (io.ReadCloser, error)
+	ContainerCreate(params *daemon.ContainerCreateConfig) (types.ContainerCreateResponse, error)
+	ContainerExecCreate(config *runconfig.ExecConfig) (string, error)
+	ContainerExecInspect(id string) (*daemon.ExecConfig, error)
+	ContainerExecResize(name string, height, width int) error
+	ContainerExecStart(name string, stdin io.ReadCloser, stdout io.Writer, stderr io.Writer) error
+	ContainerExport(name string, out io.Writer) error
+	ContainerExtractToDir(name, path string, noOverwriteDirNonDir bool, content io.Reader) error
+	ContainerInspect(name string, size bool) (*types.ContainerJSON, error)
+	ContainerInspect120(name string) (*v1p20.ContainerJSON, error)
+	// unix version
+	//ContainerInspectPre120(name string) (*v1p19.ContainerJSON, error)
+	// windows version
+	ContainerInspectPre120(name string) (*types.ContainerJSON, error)
+	ContainerKill(name string, sig uint64) error
+	ContainerLogs(containerName string, config *daemon.ContainerLogsConfig) error
+	ContainerPause(name string) error
+	ContainerRename(oldName, newName string) error
+	ContainerResize(name string, height, width int) error
+	ContainerRestart(name string, seconds int) error
+	ContainerRm(name string, config *daemon.ContainerRmConfig) error
+	Containers(config *daemon.ContainersConfig) ([]*types.Container, error)
+	ContainerStart(name string, hostConfig *runconfig.HostConfig) error
+	ContainerStatPath(name string, path string) (stat *types.ContainerPathStat, err error)
+	ContainerStats(prefixOrName string, config *daemon.ContainerStatsConfig) error
+	ContainerStop(name string, seconds int) error
+	ContainerTop(name string, psArgs string) (*types.ContainerProcessList, error)
+	ContainerUnpause(name string) error
+	ContainerWait(name string, timeout time.Duration) (int, error)
+	ContainerWsAttachWithLogs(prefixOrName string, c *daemon.ContainerWsAttachWithLogsConfig) error
+	ExecExists(name string) (bool, error)
+	Exists(id string) bool
+	IsPaused(id string) bool
+}

api/server/router/container/container.go
@@ -1,0 +1,65 @@
+package container
+
+import (
+	"github.com/docker/docker/api/server/router"
+	"github.com/docker/docker/api/server/router/local"
+)
+
+// containerRouter is a router to talk with the container controller
+type containerRouter struct {
+	backend Backend
+	routes  []router.Route
+}
+
+// NewRouter initializes a new container router
+func NewRouter(b Backend) router.Router {
+	r := &containerRouter{
+		backend: b,
+	}
+	r.initRoutes()
+	return r
+}
+
+// Routes returns the available routers to the container controller
+func (r *containerRouter) Routes() []router.Route {
+	return r.routes
+}
+
+// initRoutes initializes the routes in container router
+func (r *containerRouter) initRoutes() {
+	r.routes = []router.Route{
+		// HEAD
+		local.NewHeadRoute("/containers/{name:.*}/archive", r.headContainersArchive),
+		// GET
+		local.NewGetRoute("/containers/json", r.getContainersJSON),
+		local.NewGetRoute("/containers/{name:.*}/export", r.getContainersExport),
+		local.NewGetRoute("/containers/{name:.*}/changes", r.getContainersChanges),
+		local.NewGetRoute("/containers/{name:.*}/json", r.getContainersByName),
+		local.NewGetRoute("/containers/{name:.*}/top", r.getContainersTop),
+		local.NewGetRoute("/containers/{name:.*}/logs", r.getContainersLogs),
+		local.NewGetRoute("/containers/{name:.*}/stats", r.getContainersStats),
+		local.NewGetRoute("/containers/{name:.*}/attach/ws", r.wsContainersAttach),
+		local.NewGetRoute("/exec/{id:.*}/json", r.getExecByID),
+		local.NewGetRoute("/containers/{name:.*}/archive", r.getContainersArchive),
+		// POST
+		local.NewPostRoute("/containers/create", r.postContainersCreate),
+		local.NewPostRoute("/containers/{name:.*}/kill", r.postContainersKill),
+		local.NewPostRoute("/containers/{name:.*}/pause", r.postContainersPause),
+		local.NewPostRoute("/containers/{name:.*}/unpause", r.postContainersUnpause),
+		local.NewPostRoute("/containers/{name:.*}/restart", r.postContainersRestart),
+		local.NewPostRoute("/containers/{name:.*}/start", r.postContainersStart),
+		local.NewPostRoute("/containers/{name:.*}/stop", r.postContainersStop),
+		local.NewPostRoute("/containers/{name:.*}/wait", r.postContainersWait),
+		local.NewPostRoute("/containers/{name:.*}/resize", r.postContainersResize),
+		local.NewPostRoute("/containers/{name:.*}/attach", r.postContainersAttach),
+		local.NewPostRoute("/containers/{name:.*}/copy", r.postContainersCopy),
+		local.NewPostRoute("/containers/{name:.*}/exec", r.postContainerExecCreate),
+		local.NewPostRoute("/exec/{name:.*}/start", r.postContainerExecStart),
+		local.NewPostRoute("/exec/{name:.*}/resize", r.postContainerExecResize),
+		local.NewPostRoute("/containers/{name:.*}/rename", r.postContainerRename),
+		// PUT
+		local.NewPutRoute("/containers/{name:.*}/archive", r.putContainersArchive),
+		// DELETE
+		local.NewDeleteRoute("/containers/{name:.*}", r.deleteContainers),
+	}
+}

api/server/server.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/docker/api/server/httputils"
 	"github.com/docker/docker/api/server/router"
+	"github.com/docker/docker/api/server/router/container"
 	"github.com/docker/docker/api/server/router/local"
 	"github.com/docker/docker/api/server/router/network"
 	"github.com/docker/docker/api/server/router/volume"
@@ -172,6 +173,7 @@ func (s *Server) InitRouters(d *daemon.Daemon) {
 	s.addRouter(local.NewRouter(d))
 	s.addRouter(network.NewRouter(d))
 	s.addRouter(volume.NewRouter(d))
+	s.addRouter(container.NewRouter(d))
 
 	for _, srv := range s.servers {
 		srv.srv.Handler = s.CreateMux()

tiborvass/docker

api/server/router/container/backend_unix.go
@@ -1,0 +1,56 @@
+// +build !windows
+
+package container
+
+import (
+	"io"
+	"time"
+
+	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/api/types/versions/v1p19"
+	"github.com/tiborvass/docker/api/types/versions/v1p20"
+	"github.com/tiborvass/docker/daemon"
+	"github.com/tiborvass/docker/pkg/archive"
+	"github.com/tiborvass/docker/runconfig"
+)
+
+// Backend is all the methods that need to be implemented to provide
+// container specific functionality
+type Backend interface {
+	ContainerArchivePath(name string, path string) (content io.ReadCloser, stat *types.ContainerPathStat, err error)
+	ContainerAttachWithLogs(prefixOrName string, c *daemon.ContainerAttachWithLogsConfig) error
+	ContainerChanges(name string) ([]archive.Change, error)
+	ContainerCopy(name string, res string) (io.ReadCloser, error)
+	ContainerCreate(params *daemon.ContainerCreateConfig) (types.ContainerCreateResponse, error)
+	ContainerExecCreate(config *runconfig.ExecConfig) (string, error)
+	ContainerExecInspect(id string) (*daemon.ExecConfig, error)
+	ContainerExecResize(name string, height, width int) error
+	ContainerExecStart(name string, stdin io.ReadCloser, stdout io.Writer, stderr io.Writer) error
+	ContainerExport(name string, out io.Writer) error
+	ContainerExtractToDir(name, path string, noOverwriteDirNonDir bool, content io.Reader) error
+	ContainerInspect(name string, size bool) (*types.ContainerJSON, error)
+	ContainerInspect120(name string) (*v1p20.ContainerJSON, error)
+	// unix version
+	ContainerInspectPre120(name string) (*v1p19.ContainerJSON, error)
+	// windows version
+	//ContainerInspectPre120(name string) (*types.ContainerJSON, error)
+	ContainerKill(name string, sig uint64) error
+	ContainerLogs(containerName string, config *daemon.ContainerLogsConfig) error
+	ContainerPause(name string) error
+	ContainerRename(oldName, newName string) error
+	ContainerResize(name string, height, width int) error
+	ContainerRestart(name string, seconds int) error
+	ContainerRm(name string, config *daemon.ContainerRmConfig) error
+	Containers(config *daemon.ContainersConfig) ([]*types.Container, error)
+	ContainerStart(name string, hostConfig *runconfig.HostConfig) error
+	ContainerStatPath(name string, path string) (stat *types.ContainerPathStat, err error)
+	ContainerStats(prefixOrName string, config *daemon.ContainerStatsConfig) error
+	ContainerStop(name string, seconds int) error
+	ContainerTop(name string, psArgs string) (*types.ContainerProcessList, error)
+	ContainerUnpause(name string) error
+	ContainerWait(name string, timeout time.Duration) (int, error)
+	ContainerWsAttachWithLogs(prefixOrName string, c *daemon.ContainerWsAttachWithLogsConfig) error
+	ExecExists(name string) (bool, error)
+	Exists(id string) bool
+	IsPaused(id string) bool
+}

api/server/router/container/backend_windows.go
@@ -1,0 +1,55 @@
+// +build windows
+
+package container
+
+import (
+	"io"
+	"time"
+
+	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/api/types/versions/v1p20"
+	"github.com/tiborvass/docker/daemon"
+	"github.com/tiborvass/docker/pkg/archive"
+	"github.com/tiborvass/docker/runconfig"
+)
+
+// Backend is all the methods that need to be implemented to provide
+// container specific functionality
+type Backend interface {
+	ContainerArchivePath(name string, path string) (content io.ReadCloser, stat *types.ContainerPathStat, err error)
+	ContainerAttachWithLogs(prefixOrName string, c *daemon.ContainerAttachWithLogsConfig) error
+	ContainerChanges(name string) ([]archive.Change, error)
+	ContainerCopy(name string, res string) (io.ReadCloser, error)
+	ContainerCreate(params *daemon.ContainerCreateConfig) (types.ContainerCreateResponse, error)
+	ContainerExecCreate(config *runconfig.ExecConfig) (string, error)
+	ContainerExecInspect(id string) (*daemon.ExecConfig, error)
+	ContainerExecResize(name string, height, width int) error
+	ContainerExecStart(name string, stdin io.ReadCloser, stdout io.Writer, stderr io.Writer) error
+	ContainerExport(name string, out io.Writer) error
+	ContainerExtractToDir(name, path string, noOverwriteDirNonDir bool, content io.Reader) error
+	ContainerInspect(name string, size bool) (*types.ContainerJSON, error)
+	ContainerInspect120(name string) (*v1p20.ContainerJSON, error)
+	// unix version
+	//ContainerInspectPre120(name string) (*v1p19.ContainerJSON, error)
+	// windows version
+	ContainerInspectPre120(name string) (*types.ContainerJSON, error)
+	ContainerKill(name string, sig uint64) error
+	ContainerLogs(containerName string, config *daemon.ContainerLogsConfig) error
+	ContainerPause(name string) error
+	ContainerRename(oldName, newName string) error
+	ContainerResize(name string, height, width int) error
+	ContainerRestart(name string, seconds int) error
+	ContainerRm(name string, config *daemon.ContainerRmConfig) error
+	Containers(config *daemon.ContainersConfig) ([]*types.Container, error)
+	ContainerStart(name string, hostConfig *runconfig.HostConfig) error
+	ContainerStatPath(name string, path string) (stat *types.ContainerPathStat, err error)
+	ContainerStats(prefixOrName string, config *daemon.ContainerStatsConfig) error
+	ContainerStop(name string, seconds int) error
+	ContainerTop(name string, psArgs string) (*types.ContainerProcessList, error)
+	ContainerUnpause(name string) error
+	ContainerWait(name string, timeout time.Duration) (int, error)
+	ContainerWsAttachWithLogs(prefixOrName string, c *daemon.ContainerWsAttachWithLogsConfig) error
+	ExecExists(name string) (bool, error)
+	Exists(id string) bool
+	IsPaused(id string) bool
+}

api/server/router/container/container.go
@@ -1,0 +1,65 @@
+package container
+
+import (
+	"github.com/tiborvass/docker/api/server/router"
+	"github.com/tiborvass/docker/api/server/router/local"
+)
+
+// containerRouter is a router to talk with the container controller
+type containerRouter struct {
+	backend Backend
+	routes  []router.Route
+}
+
+// NewRouter initializes a new container router
+func NewRouter(b Backend) router.Router {
+	r := &containerRouter{
+		backend: b,
+	}
+	r.initRoutes()
+	return r
+}
+
+// Routes returns the available routers to the container controller
+func (r *containerRouter) Routes() []router.Route {
+	return r.routes
+}
+
+// initRoutes initializes the routes in container router
+func (r *containerRouter) initRoutes() {
+	r.routes = []router.Route{
+		// HEAD
+		local.NewHeadRoute("/containers/{name:.*}/archive", r.headContainersArchive),
+		// GET
+		local.NewGetRoute("/containers/json", r.getContainersJSON),
+		local.NewGetRoute("/containers/{name:.*}/export", r.getContainersExport),
+		local.NewGetRoute("/containers/{name:.*}/changes", r.getContainersChanges),
+		local.NewGetRoute("/containers/{name:.*}/json", r.getContainersByName),
+		local.NewGetRoute("/containers/{name:.*}/top", r.getContainersTop),
+		local.NewGetRoute("/containers/{name:.*}/logs", r.getContainersLogs),
+		local.NewGetRoute("/containers/{name:.*}/stats", r.getContainersStats),
+		local.NewGetRoute("/containers/{name:.*}/attach/ws", r.wsContainersAttach),
+		local.NewGetRoute("/exec/{id:.*}/json", r.getExecByID),
+		local.NewGetRoute("/containers/{name:.*}/archive", r.getContainersArchive),
+		// POST
+		local.NewPostRoute("/containers/create", r.postContainersCreate),
+		local.NewPostRoute("/containers/{name:.*}/kill", r.postContainersKill),
+		local.NewPostRoute("/containers/{name:.*}/pause", r.postContainersPause),
+		local.NewPostRoute("/containers/{name:.*}/unpause", r.postContainersUnpause),
+		local.NewPostRoute("/containers/{name:.*}/restart", r.postContainersRestart),
+		local.NewPostRoute("/containers/{name:.*}/start", r.postContainersStart),
+		local.NewPostRoute("/containers/{name:.*}/stop", r.postContainersStop),
+		local.NewPostRoute("/containers/{name:.*}/wait", r.postContainersWait),
+		local.NewPostRoute("/containers/{name:.*}/resize", r.postContainersResize),
+		local.NewPostRoute("/containers/{name:.*}/attach", r.postContainersAttach),
+		local.NewPostRoute("/containers/{name:.*}/copy", r.postContainersCopy),
+		local.NewPostRoute("/containers/{name:.*}/exec", r.postContainerExecCreate),
+		local.NewPostRoute("/exec/{name:.*}/start", r.postContainerExecStart),
+		local.NewPostRoute("/exec/{name:.*}/resize", r.postContainerExecResize),
+		local.NewPostRoute("/containers/{name:.*}/rename", r.postContainerRename),
+		// PUT
+		local.NewPutRoute("/containers/{name:.*}/archive", r.putContainersArchive),
+		// DELETE
+		local.NewDeleteRoute("/containers/{name:.*}", r.deleteContainers),
+	}
+}

api/server/server.go
@@ -10,6 +10,7 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/tiborvass/docker/api/server/httputils"
 	"github.com/tiborvass/docker/api/server/router"
+	"github.com/tiborvass/docker/api/server/router/container"
 	"github.com/tiborvass/docker/api/server/router/local"
 	"github.com/tiborvass/docker/api/server/router/network"
 	"github.com/tiborvass/docker/api/server/router/volume"
@@ -172,6 +173,7 @@ func (s *Server) InitRouters(d *daemon.Daemon) {
 	s.addRouter(local.NewRouter(d))
 	s.addRouter(network.NewRouter(d))
 	s.addRouter(volume.NewRouter(d))
+	s.addRouter(container.NewRouter(d))
 
 	for _, srv := range s.servers {
 		srv.srv.Handler = s.CreateMux()