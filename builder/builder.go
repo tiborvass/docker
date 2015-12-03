moby/moby

Move Container to its own package.
So other packages don't need to import the daemon package when they
want to use this struct.

Signed-off-by: David Calavera <david.calavera@gmail.com>

builder/builder.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	// TODO: remove dependency on daemon
+	"github.com/docker/docker/container"
 	"github.com/docker/docker/daemon"
 	"github.com/docker/docker/image"
 	"github.com/docker/docker/runconfig"
@@ -115,13 +116,11 @@ type Docker interface {
 	// Pull tells Docker to pull image referenced by `name`.
 	Pull(name string) (*image.Image, error)
 
-	// TODO: move daemon.Container to its own package
-
 	// Container looks up a Docker container referenced by `id`.
-	Container(id string) (*daemon.Container, error)
+	Container(id string) (*container.Container, error)
 	// Create creates a new Docker container and returns potential warnings
 	// TODO: put warnings in the error
-	Create(*runconfig.Config, *runconfig.HostConfig) (*daemon.Container, []string, error)
+	Create(*runconfig.Config, *runconfig.HostConfig) (*container.Container, []string, error)
 	// Remove removes a container specified by `id`.
 	Remove(id string, cfg *daemon.ContainerRmConfig) error
 	// Commit creates a new Docker image from an existing Docker container.
@@ -131,7 +130,7 @@ type Docker interface {
 	// TODO: make an Extract method instead of passing `decompress`
 	// TODO: do not pass a FileInfo, instead refactor the archive package to export a Walk function that can be used
 	// with Context.Walk
-	Copy(c *daemon.Container, destPath string, src FileInfo, decompress bool) error
+	Copy(c *container.Container, destPath string, src FileInfo, decompress bool) error
 
 	// Retain retains an image avoiding it to be removed or overwritten until a corresponding Release() call.
 	// TODO: remove
@@ -140,13 +139,13 @@ type Docker interface {
 	// TODO: remove
 	Release(sessionID string, activeImages []string)
 	// Kill stops the container execution abruptly.
-	Kill(c *daemon.Container) error
+	Kill(c *container.Container) error
 	// Mount mounts the root filesystem for the container.
-	Mount(c *daemon.Container) error
+	Mount(c *container.Container) error
 	// Unmount unmounts the root filesystem for the container.
-	Unmount(c *daemon.Container) error
+	Unmount(c *container.Container) error
 	// Start starts a new container
-	Start(c *daemon.Container) error
+	Start(c *container.Container) error
 }
 
 // ImageCache abstracts an image cache store.

daemon/export.go
@@ -3,6 +3,7 @@ package daemon
 import (
 	"io"
 
+	"github.com/docker/docker/container"
 	derr "github.com/docker/docker/errors"
 	"github.com/docker/docker/pkg/archive"
 	"github.com/docker/docker/pkg/ioutils"
@@ -29,13 +30,13 @@ func (daemon *Daemon) ContainerExport(name string, out io.Writer) error {
 	return nil
 }
 
-func (daemon *Daemon) containerExport(container *Container) (archive.Archive, error) {
+func (daemon *Daemon) containerExport(container *container.Container) (archive.Archive, error) {
 	if err := daemon.Mount(container); err != nil {
 		return nil, err
 	}
 
 	uidMaps, gidMaps := daemon.GetUIDGIDMaps()
-	archive, err := archive.TarWithOptions(container.basefs, &archive.TarOptions{
+	archive, err := archive.TarWithOptions(container.BaseFS, &archive.TarOptions{
 		Compression: archive.Uncompressed,
 		UIDMaps:     uidMaps,
 		GIDMaps:     gidMaps,

daemon/list_windows.go
@@ -1,11 +1,15 @@
 package daemon
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/docker/docker/container"
+)
 
 // excludeByIsolation is a platform specific helper function to support PS
 // filtering by Isolation. This is a Windows-only concept, so is a no-op on Unix.
-func excludeByIsolation(container *Container, ctx *listContext) iterationAction {
-	i := strings.ToLower(string(container.hostConfig.Isolation))
+func excludeByIsolation(container *container.Container, ctx *listContext) iterationAction {
+	i := strings.ToLower(string(container.HostConfig.Isolation))
 	if i == "" {
 		i = "default"
 	}

daemon/start.go
@@ -4,8 +4,8 @@ import (
 	"runtime"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/container"
 	derr "github.com/docker/docker/errors"
-	"github.com/docker/docker/pkg/promise"
 	"github.com/docker/docker/runconfig"
 )
 
@@ -16,7 +16,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 		return err
 	}
 
-	if container.isPaused() {
+	if container.IsPaused() {
 		return derr.ErrorCodeStartPaused
 	}
 
@@ -41,7 +41,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 
 	// check if hostConfig is in line with the current system settings.
 	// It may happen cgroups are umounted or the like.
-	if _, err = daemon.verifyContainerSettings(container.hostConfig, nil); err != nil {
+	if _, err = daemon.verifyContainerSettings(container.HostConfig, nil); err != nil {
 		return err
 	}
 
@@ -53,7 +53,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 }
 
 // Start starts a container
-func (daemon *Daemon) Start(container *Container) error {
+func (daemon *Daemon) Start(container *container.Container) error {
 	return daemon.containerStart(container)
 }
 
@@ -61,7 +61,7 @@ func (daemon *Daemon) Start(container *Container) error {
 // container needs, such as storage and networking, as well as links
 // between containers. The container is left waiting for a signal to
 // begin running.
-func (daemon *Daemon) containerStart(container *Container) (err error) {
+func (daemon *Daemon) containerStart(container *container.Container) (err error) {
 	container.Lock()
 	defer container.Unlock()
 
@@ -69,7 +69,7 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 		return nil
 	}
 
-	if container.removalInProgress || container.Dead {
+	if container.RemovalInProgress || container.Dead {
 		return derr.ErrorCodeContainerBeingRemoved
 	}
 
@@ -77,12 +77,12 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	// setup has been cleaned up properly
 	defer func() {
 		if err != nil {
-			container.setError(err)
+			container.SetError(err)
 			// if no one else has set it, make sure we don't leave it at zero
 			if container.ExitCode == 0 {
 				container.ExitCode = 128
 			}
-			container.toDisk()
+			container.ToDisk()
 			daemon.Cleanup(container)
 			daemon.LogContainerEvent(container, "die")
 		}
@@ -94,7 +94,7 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 
 	// Make sure NetworkMode has an acceptable value. We do this to ensure
 	// backwards API compatibility.
-	container.hostConfig = runconfig.SetDefaultNetModeIfBlank(container.hostConfig)
+	container.HostConfig = runconfig.SetDefaultNetModeIfBlank(container.HostConfig)
 
 	if err := daemon.initializeNetworking(container); err != nil {
 		return err
@@ -103,15 +103,15 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	if err != nil {
 		return err
 	}
-	if err := container.setupWorkingDirectory(); err != nil {
+	if err := container.SetupWorkingDirectory(); err != nil {
 		return err
 	}
-	env := container.createDaemonEnvironment(linkedEnv)
+	env := container.CreateDaemonEnvironment(linkedEnv)
 	if err := daemon.populateCommand(container, env); err != nil {
 		return err
 	}
 
-	if !container.hostConfig.IpcMode.IsContainer() && !container.hostConfig.IpcMode.IsHost() {
+	if !container.HostConfig.IpcMode.IsContainer() && !container.HostConfig.IpcMode.IsHost() {
 		if err := daemon.setupIpcDirs(container); err != nil {
 			return err
 		}
@@ -121,40 +121,30 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	if err != nil {
 		return err
 	}
-	mounts = append(mounts, container.ipcMounts()...)
+	mounts = append(mounts, container.IpcMounts()...)
 
-	container.command.Mounts = mounts
+	container.Command.Mounts = mounts
 	return daemon.waitForStart(container)
 }
 
-func (daemon *Daemon) waitForStart(container *Container) error {
-	container.monitor = daemon.newContainerMonitor(container, container.hostConfig.RestartPolicy)
-
-	// block until we either receive an error from the initial start of the container's
-	// process or until the process is running in the container
-	select {
-	case <-container.monitor.startSignal:
-	case err := <-promise.Go(container.monitor.Start):
-		return err
-	}
-
-	return nil
+func (daemon *Daemon) waitForStart(container *container.Container) error {
+	return container.StartMonitor(daemon, container.HostConfig.RestartPolicy)
 }
 
 // Cleanup releases any network resources allocated to the container along with any rules
 // around how containers are linked together.  It also unmounts the container's root filesystem.
-func (daemon *Daemon) Cleanup(container *Container) {
+func (daemon *Daemon) Cleanup(container *container.Container) {
 	daemon.releaseNetwork(container)
 
-	container.unmountIpcMounts(detachMounted)
+	container.UnmountIpcMounts(detachMounted)
 
 	daemon.conditionalUnmountOnCleanup(container)
 
-	for _, eConfig := range container.execCommands.s {
-		daemon.unregisterExecCommand(eConfig)
+	for _, eConfig := range container.ExecCommands.Configs() {
+		daemon.unregisterExecCommand(container, eConfig)
 	}
 
-	if err := container.unmountVolumes(false); err != nil {
+	if err := container.UnmountVolumes(false); err != nil {
 		logrus.Warnf("%s cleanup: Failed to umount volumes: %v", container.ID, err)
 	}
 }

tiborvass/docker

Signed-off-by: Tibor Vass <tibor@docker.com>

builder/builder.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	// TODO: remove dependency on daemon
+	"github.com/tiborvass/docker/container"
 	"github.com/tiborvass/docker/daemon"
 	"github.com/tiborvass/docker/image"
 	"github.com/tiborvass/docker/runconfig"
@@ -115,13 +116,11 @@ type Docker interface {
 	// Pull tells Docker to pull image referenced by `name`.
 	Pull(name string) (*image.Image, error)
 
-	// TODO: move daemon.Container to its own package
-
 	// Container looks up a Docker container referenced by `id`.
-	Container(id string) (*daemon.Container, error)
+	Container(id string) (*container.Container, error)
 	// Create creates a new Docker container and returns potential warnings
 	// TODO: put warnings in the error
-	Create(*runconfig.Config, *runconfig.HostConfig) (*daemon.Container, []string, error)
+	Create(*runconfig.Config, *runconfig.HostConfig) (*container.Container, []string, error)
 	// Remove removes a container specified by `id`.
 	Remove(id string, cfg *daemon.ContainerRmConfig) error
 	// Commit creates a new Docker image from an existing Docker container.
@@ -131,7 +130,7 @@ type Docker interface {
 	// TODO: make an Extract method instead of passing `decompress`
 	// TODO: do not pass a FileInfo, instead refactor the archive package to export a Walk function that can be used
 	// with Context.Walk
-	Copy(c *daemon.Container, destPath string, src FileInfo, decompress bool) error
+	Copy(c *container.Container, destPath string, src FileInfo, decompress bool) error
 
 	// Retain retains an image avoiding it to be removed or overwritten until a corresponding Release() call.
 	// TODO: remove
@@ -140,13 +139,13 @@ type Docker interface {
 	// TODO: remove
 	Release(sessionID string, activeImages []string)
 	// Kill stops the container execution abruptly.
-	Kill(c *daemon.Container) error
+	Kill(c *container.Container) error
 	// Mount mounts the root filesystem for the container.
-	Mount(c *daemon.Container) error
+	Mount(c *container.Container) error
 	// Unmount unmounts the root filesystem for the container.
-	Unmount(c *daemon.Container) error
+	Unmount(c *container.Container) error
 	// Start starts a new container
-	Start(c *daemon.Container) error
+	Start(c *container.Container) error
 }
 
 // ImageCache abstracts an image cache store.

daemon/export.go
@@ -3,6 +3,7 @@ package daemon
 import (
 	"io"
 
+	"github.com/tiborvass/docker/container"
 	derr "github.com/tiborvass/docker/errors"
 	"github.com/tiborvass/docker/pkg/archive"
 	"github.com/tiborvass/docker/pkg/ioutils"
@@ -29,13 +30,13 @@ func (daemon *Daemon) ContainerExport(name string, out io.Writer) error {
 	return nil
 }
 
-func (daemon *Daemon) containerExport(container *Container) (archive.Archive, error) {
+func (daemon *Daemon) containerExport(container *container.Container) (archive.Archive, error) {
 	if err := daemon.Mount(container); err != nil {
 		return nil, err
 	}
 
 	uidMaps, gidMaps := daemon.GetUIDGIDMaps()
-	archive, err := archive.TarWithOptions(container.basefs, &archive.TarOptions{
+	archive, err := archive.TarWithOptions(container.BaseFS, &archive.TarOptions{
 		Compression: archive.Uncompressed,
 		UIDMaps:     uidMaps,
 		GIDMaps:     gidMaps,

daemon/list_windows.go
@@ -1,11 +1,15 @@
 package daemon
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/tiborvass/docker/container"
+)
 
 // excludeByIsolation is a platform specific helper function to support PS
 // filtering by Isolation. This is a Windows-only concept, so is a no-op on Unix.
-func excludeByIsolation(container *Container, ctx *listContext) iterationAction {
-	i := strings.ToLower(string(container.hostConfig.Isolation))
+func excludeByIsolation(container *container.Container, ctx *listContext) iterationAction {
+	i := strings.ToLower(string(container.HostConfig.Isolation))
 	if i == "" {
 		i = "default"
 	}

daemon/start.go
@@ -4,8 +4,8 @@ import (
 	"runtime"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/tiborvass/docker/container"
 	derr "github.com/tiborvass/docker/errors"
-	"github.com/tiborvass/docker/pkg/promise"
 	"github.com/tiborvass/docker/runconfig"
 )
 
@@ -16,7 +16,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 		return err
 	}
 
-	if container.isPaused() {
+	if container.IsPaused() {
 		return derr.ErrorCodeStartPaused
 	}
 
@@ -42,7 +42,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 			if err := daemon.setHostConfig(container, hostConfig); err != nil {
 				return err
 			}
-			initDNSHostConfig(container)
+			container.InitDNSHostConfig()
 		}
 	} else {
 		if hostConfig != nil {
@@ -52,7 +52,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 
 	// check if hostConfig is in line with the current system settings.
 	// It may happen cgroups are umounted or the like.
-	if _, err = daemon.verifyContainerSettings(container.hostConfig, nil); err != nil {
+	if _, err = daemon.verifyContainerSettings(container.HostConfig, nil); err != nil {
 		return err
 	}
 
@@ -64,7 +64,7 @@ func (daemon *Daemon) ContainerStart(name string, hostConfig *runconfig.HostConf
 }
 
 // Start starts a container
-func (daemon *Daemon) Start(container *Container) error {
+func (daemon *Daemon) Start(container *container.Container) error {
 	return daemon.containerStart(container)
 }
 
@@ -72,7 +72,7 @@ func (daemon *Daemon) Start(container *Container) error {
 // container needs, such as storage and networking, as well as links
 // between containers. The container is left waiting for a signal to
 // begin running.
-func (daemon *Daemon) containerStart(container *Container) (err error) {
+func (daemon *Daemon) containerStart(container *container.Container) (err error) {
 	container.Lock()
 	defer container.Unlock()
 
@@ -80,7 +80,7 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 		return nil
 	}
 
-	if container.removalInProgress || container.Dead {
+	if container.RemovalInProgress || container.Dead {
 		return derr.ErrorCodeContainerBeingRemoved
 	}
 
@@ -88,12 +88,12 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	// setup has been cleaned up properly
 	defer func() {
 		if err != nil {
-			container.setError(err)
+			container.SetError(err)
 			// if no one else has set it, make sure we don't leave it at zero
 			if container.ExitCode == 0 {
 				container.ExitCode = 128
 			}
-			container.toDisk()
+			container.ToDisk()
 			daemon.Cleanup(container)
 			daemon.LogContainerEvent(container, "die")
 		}
@@ -105,7 +105,7 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 
 	// Make sure NetworkMode has an acceptable value. We do this to ensure
 	// backwards API compatibility.
-	container.hostConfig = runconfig.SetDefaultNetModeIfBlank(container.hostConfig)
+	container.HostConfig = runconfig.SetDefaultNetModeIfBlank(container.HostConfig)
 
 	if err := daemon.initializeNetworking(container); err != nil {
 		return err
@@ -114,15 +114,15 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	if err != nil {
 		return err
 	}
-	if err := container.setupWorkingDirectory(); err != nil {
+	if err := container.SetupWorkingDirectory(); err != nil {
 		return err
 	}
-	env := container.createDaemonEnvironment(linkedEnv)
+	env := container.CreateDaemonEnvironment(linkedEnv)
 	if err := daemon.populateCommand(container, env); err != nil {
 		return err
 	}
 
-	if !container.hostConfig.IpcMode.IsContainer() && !container.hostConfig.IpcMode.IsHost() {
+	if !container.HostConfig.IpcMode.IsContainer() && !container.HostConfig.IpcMode.IsHost() {
 		if err := daemon.setupIpcDirs(container); err != nil {
 			return err
 		}
@@ -132,10 +132,10 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	if err != nil {
 		return err
 	}
-	mounts = append(mounts, container.ipcMounts()...)
-	mounts = append(mounts, container.tmpfsMounts()...)
+	mounts = append(mounts, container.IpcMounts()...)
+	mounts = append(mounts, container.TmpfsMounts()...)
 
-	container.command.Mounts = mounts
+	container.Command.Mounts = mounts
 	if err := daemon.waitForStart(container); err != nil {
 		return err
 	}
@@ -143,34 +143,24 @@ func (daemon *Daemon) containerStart(container *Container) (err error) {
 	return nil
 }
 
-func (daemon *Daemon) waitForStart(container *Container) error {
-	container.monitor = daemon.newContainerMonitor(container, container.hostConfig.RestartPolicy)
-
-	// block until we either receive an error from the initial start of the container's
-	// process or until the process is running in the container
-	select {
-	case <-container.monitor.startSignal:
-	case err := <-promise.Go(container.monitor.Start):
-		return err
-	}
-
-	return nil
+func (daemon *Daemon) waitForStart(container *container.Container) error {
+	return container.StartMonitor(daemon, container.HostConfig.RestartPolicy)
 }
 
 // Cleanup releases any network resources allocated to the container along with any rules
 // around how containers are linked together.  It also unmounts the container's root filesystem.
-func (daemon *Daemon) Cleanup(container *Container) {
+func (daemon *Daemon) Cleanup(container *container.Container) {
 	daemon.releaseNetwork(container)
 
-	container.unmountIpcMounts(detachMounted)
+	container.UnmountIpcMounts(detachMounted)
 
 	daemon.conditionalUnmountOnCleanup(container)
 
-	for _, eConfig := range container.execCommands.Commands() {
+	for _, eConfig := range container.ExecCommands.Commands() {
 		daemon.unregisterExecCommand(container, eConfig)
 	}
 
-	if err := container.unmountVolumes(false); err != nil {
+	if err := container.UnmountVolumes(false); err != nil {
 		logrus.Warnf("%s cleanup: Failed to umount volumes: %v", container.ID, err)
 	}
 }