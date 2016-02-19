e5e5b6765e8524580ae9703d0fefc4fd788d989b
Daniel Nephin <dnephin@docker.com>
2016-02-19 17:42:51 -0500

    Build two binaries client and daemon.
    Add a proxy to support 'docker daemon'
    Fix configFile option, and remove a test that is no longer relevant.
    Remove daemon build tag.
    Remove DOCKER_CLIENTONLY from build scripts.
    
    Signed-off-by: Daniel Nephin <dnephin@docker.com>
    
    Change docker-daemon to dockerd.
    
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

---
+++ client/docker_windows.go
@@ -0,0 +1,5 @@
+package main
+
+import (
+	_ "github.com/tiborvass/docker/autogen/winresources"
+)
--- docker/daemon_none.go
+++
@@ -1,9 +0,0 @@
-// +build !daemon
-
-package main
-
-import "github.com/tiborvass/docker/cli"
-
-const daemonUsage = ""
-
-var daemonCli cli.Handler
