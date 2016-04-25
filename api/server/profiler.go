f5a2facf7d821c2aa7adb744b573aac100b0af31
Tibor Vass <tibor@docker.com>
2016-04-25 15:37:01 -0400

    pprof/trace patch to 1.10.3
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- api/server/profiler.go
+++ api/server/profiler.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/pprof"
+	"runtime"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -16,7 +18,9 @@ func profilerSetup(mainRouter *mux.Router, path string) {
 	r.HandleFunc("/pprof/cmdline", pprof.Cmdline)
 	r.HandleFunc("/pprof/profile", pprof.Profile)
 	r.HandleFunc("/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/pprof/trace", pprof.Trace)
 	r.HandleFunc("/pprof/block", pprof.Handler("block").ServeHTTP)
+	r.HandleFunc("/pprof/block/rate", blockRate)
 	r.HandleFunc("/pprof/heap", pprof.Handler("heap").ServeHTTP)
 	r.HandleFunc("/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
 	r.HandleFunc("/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
@@ -35,4 +39,19 @@ func expVars(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%q: %s", kv.Key, kv.Value)
 	})
 	fmt.Fprintf(w, "\n}\n")
+}
+
+func blockRate(w http.ResponseWriter, r *http.Request) {
+	var (
+		rate = 0
+		err  error
+	)
+	if rateString := r.FormValue("rate"); rateString != "" {
+		rate, err = strconv.Atoi(r.FormValue("rate"))
+		if err != nil {
+			http.Error(w, "rate has to be an integer, cf https://golang.org/pkg/runtime/#SetBlockProfileRate", http.StatusBadRequest)
+			return
+		}
+	}
+	runtime.SetBlockProfileRate(rate)
 }
