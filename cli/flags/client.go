7195987c5c63e4e41b374b7615115f2b7ffb3093
Daniel Nephin <dnephin@docker.com>
2016-06-21 16:42:47 -0400

    Convert dockerd to use cobra and pflag
    Signed-off-by: Daniel Nephin <dnephin@docker.com>

--- cli/flags/client.go
+++ cli/flags/client.go
@@ -1,11 +1,13 @@
 package flags
 
-import flag "github.com/tiborvass/docker/pkg/mflag"
+import (
+	"github.com/spf13/pflag"
+)
 
 // ClientFlags represents flags for the docker client.
 type ClientFlags struct {
-	FlagSet   *flag.FlagSet
-	Common    *CommonFlags
+	FlagSet   *pflag.FlagSet
+	Common    *CommonOptions
 	PostParse func()
 
 	ConfigDir string
