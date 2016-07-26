container-os/cli

Add a script to generate man pages from cobra commands.
Use the generate.sh script instead of md2man directly.
Update Dockerfile for generating man pages.

Signed-off-by: Daniel Nephin <dnephin@docker.com>
Upstream-commit: 00a8a40398263429f99b1a5f0be59048e1c6f38d
Component: engine

man/generate.go
@@ -1,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/docker/docker/cli/cobraadaptor"
+	cliflags "github.com/docker/docker/cli/flags"
+	"github.com/spf13/cobra/doc"
+)
+
+func generateManPages(path string) error {
+	header := &doc.GenManHeader{
+		Title:   "DOCKER",
+		Section: "1",
+		Source:  "Docker Community",
+	}
+	flags := &cliflags.ClientFlags{
+		Common: cliflags.InitCommonFlags(),
+	}
+	cmd := cobraadaptor.NewCobraAdaptor(flags).GetRootCommand()
+	cmd.DisableAutoGenTag = true
+	return doc.GenManTreeFromOpts(cmd, doc.GenManTreeOptions{
+		Header:           header,
+		Path:             path,
+		CommandSeparator: "-",
+	})
+}
+
+func main() {
+	path := "/tmp"
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+	fmt.Printf("Generating man pages into %s\n", path)
+	if err := generateManPages(path); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate man pages: %s\n", err.Error())
+	}
+}

tiborvass/docker

(cherry picked from commit 5b47d348d7e0bbf9464037c76cb4abd964b9ce2c)
Signed-off-by: Tibor Vass <tibor@docker.com>

man/generate.go
@@ -1,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tiborvass/docker/cli/cobraadaptor"
+	cliflags "github.com/tiborvass/docker/cli/flags"
+	"github.com/spf13/cobra/doc"
+)
+
+func generateManPages(path string) error {
+	header := &doc.GenManHeader{
+		Title:   "DOCKER",
+		Section: "1",
+		Source:  "Docker Community",
+	}
+	flags := &cliflags.ClientFlags{
+		Common: cliflags.InitCommonFlags(),
+	}
+	cmd := cobraadaptor.NewCobraAdaptor(flags).GetRootCommand()
+	cmd.DisableAutoGenTag = true
+	return doc.GenManTreeFromOpts(cmd, doc.GenManTreeOptions{
+		Header:           header,
+		Path:             path,
+		CommandSeparator: "-",
+	})
+}
+
+func main() {
+	path := "/tmp"
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+	fmt.Printf("Generating man pages into %s\n", path)
+	if err := generateManPages(path); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate man pages: %s\n", err.Error())
+	}
+}