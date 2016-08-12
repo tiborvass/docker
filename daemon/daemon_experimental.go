aduermael/cli

Make daemon events listen for plugin lifecycle events.
Signed-off-by: Anusha Ragunathan <anusha@docker.com>

daemon/daemon_experimental.go
@@ -3,6 +3,7 @@
 package daemon
 
 import (
+	"github.com/docker/docker/libcontainerd"
 	"github.com/docker/docker/plugin"
 	"github.com/docker/engine-api/types/container"
 )
@@ -11,6 +12,15 @@ func (daemon *Daemon) verifyExperimentalContainerSettings(hostConfig *container.
 	return nil, nil
 }
 
+func pluginInit(d *Daemon, cfg *Config, remote libcontainerd.Remote) error {
+	return plugin.Init(cfg.Root, remote, d.RegistryService, cfg.LiveRestore, d.LogPluginEvent)
+}
+
 func pluginShutdown() {
-	plugin.GetManager().Shutdown()
+	manager := plugin.GetManager()
+	// Check for a valid manager object. In error conditions, daemon init can fail
+	// and shutdown called, before plugin manager is initialized.
+	if manager != nil {
+		manager.Shutdown()
+	}
 }

daemon/daemon_stub.go
@@ -2,10 +2,17 @@
 
 package daemon
 
-import "github.com/docker/engine-api/types/container"
+import (
+	"github.com/docker/docker/libcontainerd"
+	"github.com/docker/engine-api/types/container"
+)
 
 func (daemon *Daemon) verifyExperimentalContainerSettings(hostConfig *container.HostConfig, config *container.Config) ([]string, error) {
 	return nil, nil
+}
+
+func pluginInit(d *Daemon, config *Config, remote libcontainerd.Remote) error {
+	return nil
 }
 
 func pluginShutdown() {

tiborvass/docker

(cherry picked from commit 14eccb790c1fa5486e398cc54ffd1b36a6d6342d)
Signed-off-by: Tibor Vass <tibor@docker.com>

daemon/daemon_experimental.go
@@ -3,6 +3,7 @@
 package daemon
 
 import (
+	"github.com/tiborvass/docker/libcontainerd"
 	"github.com/tiborvass/docker/plugin"
 	"github.com/docker/engine-api/types/container"
 )
@@ -11,6 +12,15 @@ func (daemon *Daemon) verifyExperimentalContainerSettings(hostConfig *container.
 	return nil, nil
 }
 
+func pluginInit(d *Daemon, cfg *Config, remote libcontainerd.Remote) error {
+	return plugin.Init(cfg.Root, remote, d.RegistryService, cfg.LiveRestore, d.LogPluginEvent)
+}
+
 func pluginShutdown() {
-	plugin.GetManager().Shutdown()
+	manager := plugin.GetManager()
+	// Check for a valid manager object. In error conditions, daemon init can fail
+	// and shutdown called, before plugin manager is initialized.
+	if manager != nil {
+		manager.Shutdown()
+	}
 }

daemon/daemon_stub.go
@@ -2,10 +2,17 @@
 
 package daemon
 
-import "github.com/docker/engine-api/types/container"
+import (
+	"github.com/tiborvass/docker/libcontainerd"
+	"github.com/docker/engine-api/types/container"
+)
 
 func (daemon *Daemon) verifyExperimentalContainerSettings(hostConfig *container.HostConfig, config *container.Config) ([]string, error) {
 	return nil, nil
+}
+
+func pluginInit(d *Daemon, config *Config, remote libcontainerd.Remote) error {
+	return nil
 }
 
 func pluginShutdown() {