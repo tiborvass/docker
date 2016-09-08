774e70f876e8bfad13772be4c7f45c12578e6a95
Anusha Ragunathan <anusha@docker.com>
2016-09-07 17:01:10 -0700

    Make graphdrivers work with pluginv2.
    As part of making graphdrivers support pluginv2, a PluginGetter
    interface was necessary for cleaner separation and avoiding import
    cycles.
    
    This commit creates a PluginGetter interface and makes pluginStore
    implement it. Then the pluginStore object is created in the daemon
    (rather than by the plugin manager) and passed to plugin init as
    well as to the different subsystems (eg. graphdrivers, volumedrivers).
    A side effect of this change was that some code was moved out of
    experimental. This is good, since plugin support will be stable soon.
    
    Signed-off-by: Anusha Ragunathan <anusha@docker.com>

--- volume/drivers/extpoint_test.go
+++ volume/drivers/extpoint_test.go
@@ -3,16 +3,20 @@ package volumedrivers
 import (
 	"testing"
 
+	pluginstore "github.com/tiborvass/docker/plugin/store"
 	volumetestutils "github.com/tiborvass/docker/volume/testutils"
 )
 
 func TestGetDriver(t *testing.T) {
+	pluginStore := pluginstore.NewStore("/var/lib/docker")
+	RegisterPluginGetter(pluginStore)
+
 	_, err := GetDriver("missing")
 	if err == nil {
 		t.Fatal("Expected error, was nil")
 	}
-
 	Register(volumetestutils.NewFakeDriver("fake"), "fake")
+
 	d, err := GetDriver("fake")
 	if err != nil {
 		t.Fatal(err)
