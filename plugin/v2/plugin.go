4b0f10447aa1d364f54275657368a36f9f2ae43d
Tonis Tiigi <tonistiigi@gmail.com>
2016-10-05 14:02:47 -0700

    Remove restartmanager from plugins
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- plugin/v2/plugin.go
+++ plugin/v2/plugin.go
@@ -5,16 +5,15 @@ import (
 
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/pkg/plugins"
-	"github.com/tiborvass/docker/restartmanager"
 )
 
 // Plugin represents an individual plugin.
 type Plugin struct {
 	sync.RWMutex
-	PluginObj         types.Plugin                  `json:"plugin"`
-	PClient           *plugins.Client               `json:"-"`
-	RestartManager    restartmanager.RestartManager `json:"-"`
-	RuntimeSourcePath string                        `json:"-"`
-	ExitChan          chan bool                     `json:"-"`
-	RefCount          int                           `json:"-"`
+	PluginObj         types.Plugin    `json:"plugin"`
+	PClient           *plugins.Client `json:"-"`
+	RuntimeSourcePath string          `json:"-"`
+	RefCount          int             `json:"-"`
+	Restart           bool            `json:"-"`
+	ExitChan          chan bool       `json:"-"`
 }
