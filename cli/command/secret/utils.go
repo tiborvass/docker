moby/moby

secrets: enable secret inspect and rm by secret name
Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

cli/command/secret/utils.go
@@ -1,0 +1,21 @@
+package secret
+
+import (
+	"context"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/client"
+)
+
+func getSecrets(client client.APIClient, ctx context.Context, names []string) ([]swarm.Secret, error) {
+	args := filters.NewArgs()
+	for _, n := range names {
+		args.Add("names", n)
+	}
+
+	return client.SecretList(ctx, types.SecretListOptions{
+		Filter: args,
+	})
+}

tiborvass/docker

cli/command/secret/utils.go
@@ -1,0 +1,21 @@
+package secret
+
+import (
+	"context"
+
+	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/api/types/filters"
+	"github.com/tiborvass/docker/api/types/swarm"
+	"github.com/tiborvass/docker/client"
+)
+
+func getSecrets(client client.APIClient, ctx context.Context, names []string) ([]swarm.Secret, error) {
+	args := filters.NewArgs()
+	for _, n := range names {
+		args.Add("names", n)
+	}
+
+	return client.SecretList(ctx, types.SecretListOptions{
+		Filter: args,
+	})
+}