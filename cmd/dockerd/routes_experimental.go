1b771e98a49562e614cc7a3d416bf904687ef074
Victor Vieux <vieux@docker.com>
2016-11-09 17:49:09 -0800

    move plugins out of experimental
    Signed-off-by: Victor Vieux <vieux@docker.com>

--- cmd/dockerd/routes_experimental.go
+++ cmd/dockerd/routes_experimental.go
@@ -4,14 +4,12 @@ import (
 	"github.com/tiborvass/docker/api/server/httputils"
 	"github.com/tiborvass/docker/api/server/router"
 	checkpointrouter "github.com/tiborvass/docker/api/server/router/checkpoint"
-	pluginrouter "github.com/tiborvass/docker/api/server/router/plugin"
 	"github.com/tiborvass/docker/daemon"
-	"github.com/tiborvass/docker/plugin"
 )
 
 func addExperimentalRouters(routers []router.Router, d *daemon.Daemon, decoder httputils.ContainerDecoder) []router.Router {
 	if !d.HasExperimental() {
 		return []router.Router{}
 	}
-	return append(routers, checkpointrouter.NewRouter(d, decoder), pluginrouter.NewRouter(plugin.GetManager()))
+	return append(routers, checkpointrouter.NewRouter(d, decoder))
 }
