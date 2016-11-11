cebd16349b61d8569b1848384f020db4473d09b2
Kenfe-Mickael Laventure <mickael.laventure@gmail.com>
2016-11-11 08:02:23 -0800

    Remove extra binaries commit variables from windows build
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

---
+++ daemon/info_windows.go
@@ -0,0 +1,9 @@
+package daemon
+
+import (
+	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/pkg/sysinfo"
+)
+
+func (daemon *Daemon) FillPlatformInfo(v *types.InfoBase, sysInfo *sysinfo.SysInfo) {
+}
