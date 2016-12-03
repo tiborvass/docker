aduermael/cli

libcontainerd: attach streams before create
Fix #26371

Signed-off-by: Alexander Morozov <lk4d4@docker.com>

libcontainerd/container_linux.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	containerd "github.com/docker/containerd/api/grpc/types"
+	"github.com/docker/docker/pkg/ioutils"
 	"github.com/docker/docker/restartmanager"
 	"github.com/opencontainers/runtime-spec/specs-go"
 	"golang.org/x/net/context"
@@ -91,10 +92,23 @@ func (ctr *container) start(checkpoint string, checkpointDir string) error {
 	if err != nil {
 		return nil
 	}
+	createChan := make(chan struct{})
 	iopipe, err := ctr.openFifos(spec.Process.Terminal)
 	if err != nil {
 		return err
 	}
+
+	// we need to delay stdin closure after container start or else "stdin close"
+	// event will be rejected by containerd.
+	// stdin closure happens in AttachStreams
+	stdin := iopipe.Stdin
+	iopipe.Stdin = ioutils.NewWriteCloserWrapper(stdin, func() error {
+		go func() {
+			<-createChan
+			stdin.Close()
+		}()
+		return nil
+	})
 
 	r := &containerd.CreateContainerRequest{
 		Id:            ctr.containerID,
@@ -111,17 +125,21 @@ func (ctr *container) start(checkpoint string, checkpointDir string) error {
 	}
 	ctr.client.appendContainer(ctr)
 
+	if err := ctr.client.backend.AttachStreams(ctr.containerID, *iopipe); err != nil {
+		close(createChan)
+		ctr.closeFifos(iopipe)
+		return err
+	}
+
 	resp, err := ctr.client.remote.apiClient.CreateContainer(context.Background(), r)
 	if err != nil {
+		close(createChan)
 		ctr.closeFifos(iopipe)
 		return err
 	}
 	ctr.startedAt = time.Now()
-
-	if err := ctr.client.backend.AttachStreams(ctr.containerID, *iopipe); err != nil {
-		return err
-	}
 	ctr.systemPid = systemPid(resp.Container)
+	close(createChan)
 
 	return ctr.client.backend.StateChanged(ctr.containerID, StateInfo{
 		CommonStateInfo: CommonStateInfo{

tiborvass/docker

(cherry picked from commit 8ceefb4ab5bb7915dde066426d9d27b82fe2a4d1)

libcontainerd/container_linux.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	containerd "github.com/docker/containerd/api/grpc/types"
+	"github.com/tiborvass/docker/pkg/ioutils"
 	"github.com/tiborvass/docker/restartmanager"
 	"github.com/opencontainers/specs/specs-go"
 	"github.com/tonistiigi/fifo"
@@ -92,10 +93,23 @@ func (ctr *container) start() error {
 	if err != nil {
 		return nil
 	}
+	createChan := make(chan struct{})
 	iopipe, err := ctr.openFifos(spec.Process.Terminal)
 	if err != nil {
 		return err
 	}
+
+	// we need to delay stdin closure after container start or else "stdin close"
+	// event will be rejected by containerd.
+	// stdin closure happens in AttachStreams
+	stdin := iopipe.Stdin
+	iopipe.Stdin = ioutils.NewWriteCloserWrapper(stdin, func() error {
+		go func() {
+			<-createChan
+			stdin.Close()
+		}()
+		return nil
+	})
 
 	r := &containerd.CreateContainerRequest{
 		Id:         ctr.containerID,
@@ -110,17 +124,21 @@ func (ctr *container) start() error {
 	}
 	ctr.client.appendContainer(ctr)
 
+	if err := ctr.client.backend.AttachStreams(ctr.containerID, *iopipe); err != nil {
+		close(createChan)
+		ctr.closeFifos(iopipe)
+		return err
+	}
+
 	resp, err := ctr.client.remote.apiClient.CreateContainer(context.Background(), r)
 	if err != nil {
+		close(createChan)
 		ctr.closeFifos(iopipe)
 		return err
 	}
 	ctr.startedAt = time.Now()
-
-	if err := ctr.client.backend.AttachStreams(ctr.containerID, *iopipe); err != nil {
-		return err
-	}
 	ctr.systemPid = systemPid(resp.Container)
+	close(createChan)
 
 	return ctr.client.backend.StateChanged(ctr.containerID, StateInfo{
 		CommonStateInfo: CommonStateInfo{