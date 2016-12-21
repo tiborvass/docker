5a95d7ef2be7f08bea74198b0f1469f44505030a
Vincent Demeester <vincent@sbr.pm>
2016-12-21 22:42:47 +0100

    Move names to package api
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- daemon/names.go
+++ daemon/names.go
@@ -5,16 +5,16 @@ import (
 	"strings"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/tiborvass/docker/api"
 	"github.com/tiborvass/docker/container"
 	"github.com/tiborvass/docker/pkg/namesgenerator"
 	"github.com/tiborvass/docker/pkg/registrar"
 	"github.com/tiborvass/docker/pkg/stringid"
-	"github.com/tiborvass/docker/utils"
 )
 
 var (
-	validContainerNameChars   = utils.RestrictedNameChars
-	validContainerNamePattern = utils.RestrictedNamePattern
+	validContainerNameChars   = api.RestrictedNameChars
+	validContainerNamePattern = api.RestrictedNamePattern
 )
 
 func (daemon *Daemon) registerName(container *container.Container) error {
