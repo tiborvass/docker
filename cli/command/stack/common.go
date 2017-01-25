container-os/cli

Move pkg to cli/compose/convert
Signed-off-by: Daniel Nephin <dnephin@docker.com>
Upstream-commit: 643fd775edb0a05d649f4d484e966428eb3a6b09
Component: engine

cli/command/stack/common.go
@@ -6,26 +6,26 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/cli/compose/convert"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/opts"
-	"github.com/docker/docker/pkg/composetransform"
 )
 
 func getStackFilter(namespace string) filters.Args {
 	filter := filters.NewArgs()
-	filter.Add("label", composetransform.LabelNamespace+"="+namespace)
+	filter.Add("label", convert.LabelNamespace+"="+namespace)
 	return filter
 }
 
 func getStackFilterFromOpt(namespace string, opt opts.FilterOpt) filters.Args {
 	filter := opt.Value()
-	filter.Add("label", composetransform.LabelNamespace+"="+namespace)
+	filter.Add("label", convert.LabelNamespace+"="+namespace)
 	return filter
 }
 
 func getAllStacksFilter() filters.Args {
 	filter := filters.NewArgs()
-	filter.Add("label", composetransform.LabelNamespace)
+	filter.Add("label", convert.LabelNamespace)
 	return filter
 }
 

cli/command/stack/deploy_bundlefile.go
@@ -6,7 +6,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/cli/command"
-	"github.com/docker/docker/pkg/composetransform"
+	"github.com/docker/docker/cli/compose/convert"
 )
 
 func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deployOptions) error {
@@ -19,13 +19,13 @@ func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deploy
 		return err
 	}
 
-	namespace := composetransform.NewNamespace(opts.namespace)
+	namespace := convert.NewNamespace(opts.namespace)
 
 	networks := make(map[string]types.NetworkCreate)
 	for _, service := range bundle.Services {
 		for _, networkName := range service.Networks {
 			networks[networkName] = types.NetworkCreate{
-				Labels: composetransform.AddStackLabel(namespace, nil),
+				Labels: convert.AddStackLabel(namespace, nil),
 			}
 		}
 	}
@@ -53,7 +53,7 @@ func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deploy
 		serviceSpec := swarm.ServiceSpec{
 			Annotations: swarm.Annotations{
 				Name:   name,
-				Labels: composetransform.AddStackLabel(namespace, service.Labels),
+				Labels: convert.AddStackLabel(namespace, service.Labels),
 			},
 			TaskTemplate: swarm.TaskSpec{
 				ContainerSpec: swarm.ContainerSpec{
@@ -64,7 +64,7 @@ func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deploy
 					// Service Labels will not be copied to Containers
 					// automatically during the deployment so we apply
 					// it here.
-					Labels: composetransform.AddStackLabel(namespace, nil),
+					Labels: convert.AddStackLabel(namespace, nil),
 				},
 			},
 			EndpointSpec: &swarm.EndpointSpec{

cli/command/stack/list.go
@@ -11,8 +11,8 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/cli"
 	"github.com/docker/docker/cli/command"
+	"github.com/docker/docker/cli/compose/convert"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/composetransform"
 	"github.com/spf13/cobra"
 )
 
@@ -90,10 +90,10 @@ func getStacks(
 	m := make(map[string]*stack, 0)
 	for _, service := range services {
 		labels := service.Spec.Labels
-		name, ok := labels[composetransform.LabelNamespace]
+		name, ok := labels[convert.LabelNamespace]
 		if !ok {
 			return nil, fmt.Errorf("cannot get label %s for service %s",
-				composetransform.LabelNamespace, service.ID)
+				convert.LabelNamespace, service.ID)
 		}
 		ztack, ok := m[name]
 		if !ok {

tiborvass/docker

cli/command/stack/common.go
@@ -6,26 +6,26 @@ import (
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/api/types/filters"
 	"github.com/tiborvass/docker/api/types/swarm"
+	"github.com/tiborvass/docker/cli/compose/convert"
 	"github.com/tiborvass/docker/client"
 	"github.com/tiborvass/docker/opts"
-	"github.com/tiborvass/docker/pkg/composetransform"
 )
 
 func getStackFilter(namespace string) filters.Args {
 	filter := filters.NewArgs()
-	filter.Add("label", composetransform.LabelNamespace+"="+namespace)
+	filter.Add("label", convert.LabelNamespace+"="+namespace)
 	return filter
 }
 
 func getStackFilterFromOpt(namespace string, opt opts.FilterOpt) filters.Args {
 	filter := opt.Value()
-	filter.Add("label", composetransform.LabelNamespace+"="+namespace)
+	filter.Add("label", convert.LabelNamespace+"="+namespace)
 	return filter
 }
 
 func getAllStacksFilter() filters.Args {
 	filter := filters.NewArgs()
-	filter.Add("label", composetransform.LabelNamespace)
+	filter.Add("label", convert.LabelNamespace)
 	return filter
 }
 

cli/command/stack/deploy_bundlefile.go
@@ -6,7 +6,7 @@ import (
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/api/types/swarm"
 	"github.com/tiborvass/docker/cli/command"
-	"github.com/tiborvass/docker/pkg/composetransform"
+	"github.com/tiborvass/docker/cli/compose/convert"
 )
 
 func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deployOptions) error {
@@ -19,13 +19,13 @@ func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deploy
 		return err
 	}
 
-	namespace := composetransform.NewNamespace(opts.namespace)
+	namespace := convert.NewNamespace(opts.namespace)
 
 	networks := make(map[string]types.NetworkCreate)
 	for _, service := range bundle.Services {
 		for _, networkName := range service.Networks {
 			networks[networkName] = types.NetworkCreate{
-				Labels: composetransform.AddStackLabel(namespace, nil),
+				Labels: convert.AddStackLabel(namespace, nil),
 			}
 		}
 	}
@@ -53,7 +53,7 @@ func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deploy
 		serviceSpec := swarm.ServiceSpec{
 			Annotations: swarm.Annotations{
 				Name:   name,
-				Labels: composetransform.AddStackLabel(namespace, service.Labels),
+				Labels: convert.AddStackLabel(namespace, service.Labels),
 			},
 			TaskTemplate: swarm.TaskSpec{
 				ContainerSpec: swarm.ContainerSpec{
@@ -64,7 +64,7 @@ func deployBundle(ctx context.Context, dockerCli *command.DockerCli, opts deploy
 					// Service Labels will not be copied to Containers
 					// automatically during the deployment so we apply
 					// it here.
-					Labels: composetransform.AddStackLabel(namespace, nil),
+					Labels: convert.AddStackLabel(namespace, nil),
 				},
 			},
 			EndpointSpec: &swarm.EndpointSpec{

cli/command/stack/list.go
@@ -11,8 +11,8 @@ import (
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/cli"
 	"github.com/tiborvass/docker/cli/command"
+	"github.com/tiborvass/docker/cli/compose/convert"
 	"github.com/tiborvass/docker/client"
-	"github.com/tiborvass/docker/pkg/composetransform"
 	"github.com/spf13/cobra"
 )
 
@@ -90,10 +90,10 @@ func getStacks(
 	m := make(map[string]*stack, 0)
 	for _, service := range services {
 		labels := service.Spec.Labels
-		name, ok := labels[composetransform.LabelNamespace]
+		name, ok := labels[convert.LabelNamespace]
 		if !ok {
 			return nil, fmt.Errorf("cannot get label %s for service %s",
-				composetransform.LabelNamespace, service.ID)
+				convert.LabelNamespace, service.ID)
 		}
 		ztack, ok := m[name]
 		if !ok {