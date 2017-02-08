6f08a42b7556d9972562ca42c62669ee1cc50c14
Tibor Vass <tibor@docker.com>
2017-02-07 18:30:06 -0800

    fix vendoring in yaml/
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- yaml/generate.sh
+++ yaml/generate.sh
@@ -8,5 +8,5 @@ set -eu
 mkdir -p ./yaml/yaml1
 
 # Generate man pages from cobra commands
-go build -o /tmp/gen-yamlfiles ./yaml
+go build -o /tmp/gen-yamlfiles yaml
 /tmp/gen-yamlfiles --root . --target ./yaml/yaml1
