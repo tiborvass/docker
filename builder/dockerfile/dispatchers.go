moby/moby

Allow ARG to come before FROM to support variables in FROM.
Signed-off-by: Daniel Nephin <dnephin@docker.com>

builder/dockerfile/dispatchers.go
@@ -23,6 +23,7 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/strslice"
 	"github.com/docker/docker/builder"
+	"github.com/docker/docker/pkg/shellvar"
 	"github.com/docker/docker/pkg/signal"
 	"github.com/docker/go-connections/nat"
 )
@@ -201,7 +202,17 @@ func from(b *Builder, args []string, attributes map[string]bool, original string
 		return err
 	}
 
-	name := args[0]
+	getBuildArg := func(key string) (string, bool) {
+		value, ok := b.options.BuildArgs[key]
+		if value != nil {
+			return *value, ok
+		}
+		return "", ok
+	}
+	name, err := shellvar.Substitute(args[0], getBuildArg)
+	if err != nil {
+		return err
+	}
 
 	var image builder.Image
 
@@ -334,7 +345,7 @@ func workdir(b *Builder, args []string, attributes map[string]bool, original str
 // RUN [ "echo", "hi" ] # echo hi
 //
 func run(b *Builder, args []string, attributes map[string]bool, original string) error {
-	if b.image == "" && !b.noBaseImage {
+	if !b.hasFromImage() {
 		return errors.New("Please provide a source image with `from` prior to run")
 	}
 
@@ -741,6 +752,10 @@ func arg(b *Builder, args []string, attributes map[string]bool, original string)
 		b.options.BuildArgs[name] = &newValue
 	}
 
+	// Arg before FROM doesn't add a layer
+	if !b.hasFromImage() {
+		return nil
+	}
 	return b.commit("", b.runConfig.Cmd, fmt.Sprintf("ARG %s", arg))
 }
 

tiborvass/docker

builder/dockerfile/dispatchers.go
@@ -22,6 +22,7 @@ import (
 	"github.com/tiborvass/docker/api/types/container"
 	"github.com/tiborvass/docker/api/types/strslice"
 	"github.com/tiborvass/docker/builder"
+	"github.com/tiborvass/docker/pkg/shellvar"
 	"github.com/tiborvass/docker/pkg/signal"
 	"github.com/docker/go-connections/nat"
 	"github.com/pkg/errors"
@@ -218,7 +219,17 @@ func from(b *Builder, args []string, attributes map[string]bool, original string
 		return err
 	}
 
-	name := args[0]
+	getBuildArg := func(key string) (string, bool) {
+		value, ok := b.options.BuildArgs[key]
+		if value != nil {
+			return *value, ok
+		}
+		return "", ok
+	}
+	name, err := shellvar.Substitute(args[0], getBuildArg)
+	if err != nil {
+		return err
+	}
 
 	var image builder.Image
 
@@ -360,7 +371,7 @@ func workdir(b *Builder, args []string, attributes map[string]bool, original str
 // RUN [ "echo", "hi" ] # echo hi
 //
 func run(b *Builder, args []string, attributes map[string]bool, original string) error {
-	if b.image == "" && !b.noBaseImage {
+	if !b.hasFromImage() {
 		return errors.New("Please provide a source image with `from` prior to run")
 	}
 
@@ -790,6 +801,10 @@ func arg(b *Builder, args []string, attributes map[string]bool, original string)
 	}
 	b.allowedBuildArgs[name] = value
 
+	// Arg before FROM doesn't add a layer
+	if !b.hasFromImage() {
+		return nil
+	}
 	return b.commit("", b.runConfig.Cmd, fmt.Sprintf("ARG %s", arg))
 }
 