014f6e6fbcc3b6df2f6c06bd4ca7ae610996bc00
Vincent Demeester <vincent@sbr.pm>
2017-05-29 16:22:33 -0700

    Move cli.Configdir away (in `flags`).
    This makes integration not depend anymore of `cli` and thus not
    require `cobra` and other packages to compile.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- integration-cli/check_test.go
+++ integration-cli/check_test.go
@@ -12,7 +12,7 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types/swarm"
-	dcli "github.com/tiborvass/docker/cli"
+	"github.com/tiborvass/docker/cli/flags"
 	"github.com/tiborvass/docker/integration-cli/cli"
 	"github.com/tiborvass/docker/integration-cli/cli/build/fakestorage"
 	"github.com/tiborvass/docker/integration-cli/daemon"
@@ -406,7 +406,7 @@ func (s *DockerTrustSuite) TearDownTest(c *check.C) {
 	}
 
 	// Remove trusted keys and metadata after test
-	os.RemoveAll(filepath.Join(dcli.ConfigurationDir(), "trust"))
+	os.RemoveAll(filepath.Join(flags.ConfigurationDir(), "trust"))
 	s.ds.TearDownTest(c)
 }
 
--- integration-cli/trust_server_test.go
+++ integration-cli/trust_server_test.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	dcli "github.com/tiborvass/docker/cli"
+	"github.com/tiborvass/docker/cli/flags"
 	"github.com/tiborvass/docker/integration-cli/checker"
 	"github.com/tiborvass/docker/integration-cli/cli"
 	icmd "github.com/tiborvass/docker/pkg/testutil/cmd"
@@ -108,7 +108,7 @@ func newTestNotary(c *check.C) (*testNotary, error) {
 		"skipTLSVerify": true
 	}
 }`
-	if _, err = fmt.Fprintf(clientConfig, template, filepath.Join(dcli.ConfigurationDir(), "trust"), notaryURL); err != nil {
+	if _, err = fmt.Fprintf(clientConfig, template, filepath.Join(flags.ConfigurationDir(), "trust"), notaryURL); err != nil {
 		os.RemoveAll(tmp)
 		return nil, err
 	}
