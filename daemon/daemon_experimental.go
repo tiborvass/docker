cad52a74796fd6ab170034d6a9ae98ebafc825e8
Sebastiaan van Stijn <github@gone.nl>
2017-12-18 17:07:48 +0100

    Remove unused experimental code
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/daemon_experimental.go
+++
@@ -1,7 +0,0 @@
-package daemon
-
-import "github.com/tiborvass/docker/api/types/container"
-
-func (daemon *Daemon) verifyExperimentalContainerSettings(hostConfig *container.HostConfig, config *container.Config) ([]string, error) {
-	return nil, nil
-}
