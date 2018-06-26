3a03d617c8f0d78be98ee77d12958c321c5c1eac
Tonis Tiigi <tonistiigi@gmail.com>
2018-06-26 11:30:19 -0700

    builder: buildkit rebase update
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>

--- pkg/system/lcow.go
+++ pkg/system/lcow.go
@@ -2,6 +2,7 @@ package system // import "github.com/tiborvass/docker/pkg/system"
 
 import (
 	"runtime"
+	"strings"
 )
 
 // IsOSSupported determines if an operating system is supported by the host
