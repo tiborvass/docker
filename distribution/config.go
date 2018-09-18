5ec453f59c722231f9c8a5ee5a4d0bba1752ea7e
Justin Cormack <justin.cormack@docker.com>
2018-09-18 16:14:04 +0100

    Remove the rest of v1 manifest support
    As people are using the UUID in `docker info` that was based on the v1 manifest signing key, replace
    with a UUID instead.
    
    Remove deprecated `--disable-legacy-registry` option that was scheduled to be removed in 18.03.
    
    Signed-off-by: Justin Cormack <justin.cormack@docker.com>

--- distribution/config.go
+++ distribution/config.go
@@ -18,7 +18,6 @@ import (
 	"github.com/tiborvass/docker/pkg/system"
 	refstore "github.com/tiborvass/docker/reference"
 	"github.com/tiborvass/docker/registry"
-	"github.com/docker/libtrust"
 	"github.com/opencontainers/go-digest"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
@@ -73,9 +72,6 @@ type ImagePushConfig struct {
 	ConfigMediaType string
 	// LayerStores (indexed by operating system) manages layers.
 	LayerStores map[string]PushLayerProvider
-	// TrustKey is the private key for legacy signatures. This is typically
-	// an ephemeral key, since these signatures are no longer verified.
-	TrustKey libtrust.PrivateKey
 	// UploadManager dispatches uploads.
 	UploadManager *xfer.LayerUploadManager
 }
