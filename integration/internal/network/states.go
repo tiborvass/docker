a9fba638cddfa2d5c7f240c8b4be3a32c491d700
Yong Tang <yong.tang.github@outlook.com>
2019-01-12 22:59:49 +0000

    Remove code duplication and consolidate networkIsRemoved
    This fix removes code duplication and consolidates networkIsRemoved
    into one place.
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

---
+++ integration/internal/network/states.go
@@ -0,0 +1,20 @@
+package network
+
+import (
+	"context"
+
+	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/client"
+	"gotest.tools/poll"
+)
+
+// IsRemoved verifies the network is removed.
+func IsRemoved(ctx context.Context, client client.NetworkAPIClient, networkID string) func(log poll.LogT) poll.Result {
+	return func(log poll.LogT) poll.Result {
+		_, err := client.NetworkInspect(ctx, networkID, types.NetworkInspectOptions{})
+		if err == nil {
+			return poll.Continue("waiting for network %s to be removed", networkID)
+		}
+		return poll.Success()
+	}
+}
