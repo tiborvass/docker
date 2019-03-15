moby/moby

Update tests to check returned errors
Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

client/config_list_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/errdefs"
 	"gotest.tools/assert"
 	is "gotest.tools/assert/cmp"
 )
@@ -35,6 +36,9 @@ func TestConfigListError(t *testing.T) {
 	_, err := client.ConfigList(context.Background(), types.ConfigListOptions{})
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/container_diff_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/errdefs"
 )
 
 func TestContainerDiffError(t *testing.T) {
@@ -21,7 +22,9 @@ func TestContainerDiffError(t *testing.T) {
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
 	}
-
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
+	}
 }
 
 func TestContainerDiff(t *testing.T) {

client/container_inspect_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/errdefs"
 	"github.com/pkg/errors"
 )
 
@@ -22,6 +23,9 @@ func TestContainerInspectError(t *testing.T) {
 	_, err := client.ContainerInspect(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/container_unpause_test.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+
+	"github.com/docker/docker/errdefs"
 )
 
 func TestContainerUnpauseError(t *testing.T) {
@@ -17,6 +19,9 @@ func TestContainerUnpauseError(t *testing.T) {
 	err := client.ContainerUnpause(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/info_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/errdefs"
 )
 
 func TestInfoServerError(t *testing.T) {
@@ -20,6 +21,9 @@ func TestInfoServerError(t *testing.T) {
 	_, err := client.Info(context.Background())
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/plugin_inspect_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/errdefs"
 	"github.com/pkg/errors"
 )
 
@@ -22,6 +23,9 @@ func TestPluginInspectError(t *testing.T) {
 	_, _, err := client.PluginInspectWithRaw(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/service_list_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/errdefs"
 )
 
 func TestServiceListError(t *testing.T) {
@@ -23,6 +24,9 @@ func TestServiceListError(t *testing.T) {
 	_, err := client.ServiceList(context.Background(), types.ServiceListOptions{})
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/task_inspect_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/errdefs"
 	"github.com/pkg/errors"
 )
 
@@ -22,6 +23,9 @@ func TestTaskInspectError(t *testing.T) {
 	_, _, err := client.TaskInspectWithRaw(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

tiborvass/docker

client/config_list_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/api/types/filters"
 	"github.com/tiborvass/docker/api/types/swarm"
+	"github.com/tiborvass/docker/errdefs"
 	"gotest.tools/assert"
 	is "gotest.tools/assert/cmp"
 )
@@ -35,6 +36,9 @@ func TestConfigListError(t *testing.T) {
 	_, err := client.ConfigList(context.Background(), types.ConfigListOptions{})
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/container_diff_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types/container"
+	"github.com/tiborvass/docker/errdefs"
 )
 
 func TestContainerDiffError(t *testing.T) {
@@ -21,7 +22,9 @@ func TestContainerDiffError(t *testing.T) {
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
 	}
-
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
+	}
 }
 
 func TestContainerDiff(t *testing.T) {

client/container_inspect_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/errdefs"
 	"github.com/pkg/errors"
 )
 
@@ -22,6 +23,9 @@ func TestContainerInspectError(t *testing.T) {
 	_, err := client.ContainerInspect(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/container_unpause_test.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+
+	"github.com/tiborvass/docker/errdefs"
 )
 
 func TestContainerUnpauseError(t *testing.T) {
@@ -17,6 +19,9 @@ func TestContainerUnpauseError(t *testing.T) {
 	err := client.ContainerUnpause(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/info_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/errdefs"
 )
 
 func TestInfoServerError(t *testing.T) {
@@ -20,6 +21,9 @@ func TestInfoServerError(t *testing.T) {
 	_, err := client.Info(context.Background())
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/plugin_inspect_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/errdefs"
 	"github.com/pkg/errors"
 )
 
@@ -22,6 +23,9 @@ func TestPluginInspectError(t *testing.T) {
 	_, _, err := client.PluginInspectWithRaw(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/service_list_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/tiborvass/docker/api/types"
 	"github.com/tiborvass/docker/api/types/filters"
 	"github.com/tiborvass/docker/api/types/swarm"
+	"github.com/tiborvass/docker/errdefs"
 )
 
 func TestServiceListError(t *testing.T) {
@@ -23,6 +24,9 @@ func TestServiceListError(t *testing.T) {
 	_, err := client.ServiceList(context.Background(), types.ServiceListOptions{})
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 

client/task_inspect_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 
 	"github.com/tiborvass/docker/api/types/swarm"
+	"github.com/tiborvass/docker/errdefs"
 	"github.com/pkg/errors"
 )
 
@@ -22,6 +23,9 @@ func TestTaskInspectError(t *testing.T) {
 	_, _, err := client.TaskInspectWithRaw(context.Background(), "nothing")
 	if err == nil || err.Error() != "Error response from daemon: Server error" {
 		t.Fatalf("expected a Server Error, got %v", err)
+	}
+	if !errdefs.IsSystem(err) {
+		t.Fatalf("expected a Server Error, got %T", err)
 	}
 }
 