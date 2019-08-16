a3ee925d60724fb5efe9e6d4b7272d4a70dbb494
Tibor Vass <tibor@docker.com>
2019-08-16 20:53:57 +0000

    checker
    Signed-off-by: Tibor Vass <tibor@docker.com>

--- integration-cli/checker/checker.go
+++ integration-cli/checker/checker.go
@@ -2,23 +2,69 @@
 package checker // import "github.com/docker/docker/integration-cli/checker"
 
 import (
-	"github.com/go-check/check"
+	"fmt"
+
 	"github.com/vdemeester/shakers"
+	"gotest.tools/assert"
+	"gotest.tools/assert/cmp"
 )
 
 // As a commodity, we bring all check.Checker variables into the current namespace to avoid having
 // to think about check.X versus checker.X.
 var (
-	DeepEquals = check.DeepEquals
-	HasLen     = check.HasLen
-	IsNil      = check.IsNil
-	Matches    = check.Matches
-	Not        = check.Not
-	NotNil     = check.NotNil
-
-	Contains    = shakers.Contains
-	Equals      = shakers.Equals
-	False       = shakers.False
 	GreaterThan = shakers.GreaterThan
-	True        = shakers.True
 )
+
+type Compare func(x interface{}) assert.BoolOrComparison
+
+func False(x interface{}) assert.BoolOrComparison {
+	return !x.(bool)
+}
+
+func True(x interface{}) assert.BoolOrComparison {
+	return x
+}
+
+func Equals(y interface{}) Compare {
+	return func(x interface{}) assert.BoolOrComparison {
+		return cmp.Equal(x, y)
+	}
+}
+
+func Contains(y interface{}) Compare {
+	return func(x interface{}) assert.BoolOrComparison {
+		return cmp.Contains(x, y)
+	}
+}
+
+func Not(c Compare) Compare {
+	return func(x interface{}) assert.BoolOrComparison {
+		r := c(x)
+		switch r := r.(type) {
+		case bool:
+			return !r
+		case cmp.Comparison:
+			return !r().Success()
+		default:
+			panic(fmt.Sprintf("unexpected type %T", r))
+		}
+	}
+}
+
+func DeepEquals(y interface{}) Compare {
+	return func(x interface{}) assert.BoolOrComparison {
+		return cmp.DeepEqual(x, y)
+	}
+}
+
+func HasLen(y int) Compare {
+	return func(x interface{}) assert.BoolOrComparison {
+		return cmp.Len(x, y)
+	}
+}
+
+func IsNil(x interface{}) assert.BoolOrComparison {
+	return cmp.Nil(x)
+}
+
+var NotNil Compare = Not(IsNil)
