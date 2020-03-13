2f25faece05825314bf669bcdd08dff7d8c5caef
Kir Kolyshkin <kolyshkin@gmail.com>
2020-03-13 15:51:55 -0700

    pkg/mount: make it shallow
    Make pkg/mount a shallow package, relying on
    github.com/moby/sys/mount and github.com/moby/sys/mountinfo
    
    The plan is to
     - switch the rest of this repo to use moby/sys directly
     - add deprecation notice to pkg/mount
     - (eventually) remove pkg/mount
    
    The nolint:golint annotation is added to suppress warnings like
    "exported XXX should have comment or be unexported".
    
    The ForceMount function is deprecated (and is identical to Mount),
    so let's not use it (but still provide an alias for those who do).
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- pkg/mount/mounter_unsupported.go
+++
@@ -1,7 +0,0 @@
-// +build !linux,!freebsd freebsd,!cgo
-
-package mount // import "github.com/tiborvass/docker/pkg/mount"
-
-func mount(device, target, mType string, flag uintptr, data string) error {
-	panic("Not implemented")
-}
