silvin-lubecki/docker-ce

import system for plugin/manager.go
To get the cherry-pick ba42966 to merge smoothly,
expecting import of system. The import of system was added by
https://github.com/docker/docker-ce/commit/8508f49 but not taking
entire commit.

Signed-off-by: Andrew Hsu <andrewhsu@docker.com>

plugin/manager.go
@@ -21,6 +21,7 @@ import (
 	"github.com/docker/docker/pkg/authorization"
 	"github.com/docker/docker/pkg/ioutils"
 	"github.com/docker/docker/pkg/mount"
+	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/plugin/v2"
 	"github.com/docker/docker/registry"
 	"github.com/opencontainers/go-digest"

tiborvass/docker

plugin/manager.go
@@ -21,6 +21,7 @@ import (
 	"github.com/tiborvass/docker/pkg/authorization"
 	"github.com/tiborvass/docker/pkg/ioutils"
 	"github.com/tiborvass/docker/pkg/mount"
+	"github.com/tiborvass/docker/pkg/system"
 	"github.com/tiborvass/docker/plugin/v2"
 	"github.com/tiborvass/docker/registry"
 	"github.com/opencontainers/go-digest"