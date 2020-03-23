container-os/cli

Fix ONBUILD COPY
the source was missing from the second dispatch

Signed-off-by: Daniel Nephin <dnephin@docker.com>
Upstream-commit: 3f2604157790408acf5ad05c74cebe105f2b6979
Component: engine

integration-cli/cli/build/fakecontext/context.go
@@ -2,9 +2,12 @@ package fakecontext
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/docker/docker/pkg/archive"
 )
 
 type testingT interface {
@@ -109,4 +112,13 @@ func (f *Fake) Delete(file string) error {
 // Close deletes the context
 func (f *Fake) Close() error {
 	return os.RemoveAll(f.Dir)
+}
+
+// AsTarReader returns a ReadCloser with the contents of Dir as a tar archive.
+func (f *Fake) AsTarReader(t testingT) io.ReadCloser {
+	reader, err := archive.TarWithOptions(f.Dir, &archive.TarOptions{})
+	if err != nil {
+		t.Fatalf("Failed to create tar from %s: %s", f.Dir, err)
+	}
+	return reader
 }

tiborvass/docker

(cherry picked from commit 858c5511904dab47881a81f043dc23a0a1b63de0)

integration-cli/cli/build/fakecontext/context.go
@@ -2,9 +2,12 @@ package fakecontext
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/tiborvass/docker/pkg/archive"
 )
 
 type testingT interface {
@@ -109,4 +112,13 @@ func (f *Fake) Delete(file string) error {
 // Close deletes the context
 func (f *Fake) Close() error {
 	return os.RemoveAll(f.Dir)
+}
+
+// AsTarReader returns a ReadCloser with the contents of Dir as a tar archive.
+func (f *Fake) AsTarReader(t testingT) io.ReadCloser {
+	reader, err := archive.TarWithOptions(f.Dir, &archive.TarOptions{})
+	if err != nil {
+		t.Fatalf("Failed to create tar from %s: %s", f.Dir, err)
+	}
+	return reader
 }