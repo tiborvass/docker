4c198a5afc6413ee1f643fbb70d76ad5628b6d37
Brian Goff <cpuguy83@gmail.com>
2020-04-08 12:24:31 -0700

    Reduce allocations for logfile reader
    Before this change, the log decoder function provided by the log driver
    to logfile would not be able to re-use buffers, causing undeeded
    allocations and memory bloat for dockerd.
    
    This change introduces an interface that allows the log driver to manage
    it's memory usge more effectively.
    This only affects json-file and local log drivers.
    
    `json-file` still is not great just because of how the json decoder in the
    stdlib works.
    `local` is significantly improved.
    
    Signed-off-by: Brian Goff <cpuguy83@gmail.com>

--- daemon/logger/local/local_test.go
+++ daemon/logger/local/local_test.go
@@ -1,20 +1,17 @@
 package local
 
 import (
+	"bytes"
 	"context"
 	"encoding/binary"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
-
-	"bytes"
-	"fmt"
-
-	"strings"
-
-	"io"
 
 	"github.com/tiborvass/docker/api/types/backend"
 	"github.com/tiborvass/docker/api/types/plugins/logdriver"
