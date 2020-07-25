b471d0cd153cbb9e15bbfb6d9f1843ab32b1dc09
Sebastiaan van Stijn <github@gone.nl>
2020-07-25 14:14:38 +0200

    API: add "prune" events
    This patch adds a new "prune" event type to indicate that pruning of a resource
    type completed.
    
    This event-type can be used on systems that want to perform actions after
    resources have been cleaned up. For example, Docker Desktop performs an fstrim
    after resources are deleted (https://github.com/linuxkit/linuxkit/tree/v0.7/pkg/trim-after-delete).
    
    While the current (remove, destroy) events can provide information on _most_
    resources, there is currently no event triggered after the BuildKit build-cache
    is cleaned.
    
    Prune events have a `reclaimed` attribute, indicating the amount of space that
    was reclaimed (in bytes). The attribute can be used, for example, to use as a
    threshold for performing fstrim actions. Reclaimed space for `network` events
    will always be 0, but the field is added to be consistent with prune events for
    other resources.
    
    To test this patch:
    
    Create some resources:
    
        for i in foo bar baz; do \
            docker network create network_$i \
            && docker volume create volume_$i \
            && docker run -d --name container_$i -v volume_$i:/volume busybox sh -c 'truncate -s 5M somefile; truncate -s 5M /volume/file' \
            && docker tag busybox:latest image_$i; \
        done;
    
        docker pull alpine
        docker pull nginx:alpine
    
        echo -e "FROM busybox\nRUN truncate -s 50M bigfile" | DOCKER_BUILDKIT=1 docker build -
    
    Start listening for "prune" events in another shell:
    
        docker events --filter event=prune
    
    Prune containers, networks, volumes, and build-cache:
    
        docker system prune -af --volumes
    
    See the events that are returned:
    
        docker events --filter event=prune
        2020-07-25T12:12:09.268491000Z container prune  (reclaimed=15728640)
        2020-07-25T12:12:09.447890400Z network prune  (reclaimed=0)
        2020-07-25T12:12:09.452323000Z volume prune  (reclaimed=15728640)
        2020-07-25T12:12:09.517236200Z image prune  (reclaimed=21568540)
        2020-07-25T12:12:09.566662600Z builder prune  (reclaimed=52428841)
    
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- daemon/images/image_prune.go
+++ daemon/images/image_prune.go
@@ -3,11 +3,13 @@ package images // import "github.com/tiborvass/docker/daemon/images"
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"sync/atomic"
 	"time"
 
 	"github.com/docker/distribution/reference"
 	"github.com/tiborvass/docker/api/types"
+	"github.com/tiborvass/docker/api/types/events"
 	"github.com/tiborvass/docker/api/types/filters"
 	timetypes "github.com/tiborvass/docker/api/types/time"
 	"github.com/tiborvass/docker/errdefs"
@@ -159,7 +161,11 @@ deleteImagesLoop:
 	if canceled {
 		logrus.Debugf("ImagesPrune operation cancelled: %#v", *rep)
 	}
-
+	i.eventsService.Log("prune", events.ImageEventType, events.Actor{
+		Attributes: map[string]string{
+			"reclaimed": strconv.FormatUint(rep.SpaceReclaimed, 10),
+		},
+	})
 	return rep, nil
 }
 
